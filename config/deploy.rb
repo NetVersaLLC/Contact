cd8e0ccba3fdf1a35e8f6a88965acdb6d359fb16
Jonathan Jeffus <jjeffus@gmail.com>
2012-04-30 14:08:34 -0500

    fixing deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,21 @@
 require "bundler/capistrano"
+load "deploy/assets"
 
 #set :ssh_options, {
 #  :keys => "C:\\msysgit\\.ssh\\id_rsa"
 #}
+
 set :application, "Contact"
 set :repository,  "git@github.com:jjeffus/Contact.git"
 set :scm, :git
 set :user, 'deploy'
-set :deploy_to, '/home/deploy/public_html'
+set :deploy_to, '/home/deploy/contact'
 set :keep_releases, 5
 server "franklin.netversa.com", :app, :web, :db, :primary => true
+set :default_shell, "bash -l"
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user
+set :use_sudo   , false
 
 namespace :deploy do
   desc "Start the Thin processes"
