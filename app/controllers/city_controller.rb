52229b91d1fd05d8e63cadfdeb75d8d7cbf36435
Jonathan Jeffus <jjeffus@gmail.com>
2012-06-13 17:12:13 -0500

    fix zip naming issues

--- app/controllers/city_controller.rb
+++ app/controllers/city_controller.rb
@@ -2,7 +2,7 @@ class CityController < ApplicationController
   before_filter :authenticate_user!
   respond_to    :js
   def index
-    @zips = Zip.where('state = ? AND city LIKE ?', params[:state], "#{params[:term]}%").order(:city).group(:city).limit(10)
+    @zips = Location.where('state = ? AND city LIKE ?', params[:state], "#{params[:term]}%").order(:city).group(:city).limit(10)
     respond_with(@zips)
   end
 end
