a41d86ec194416e50ada959d890ed33b730f3889
Jonathan Jeffus <jonathan@blazingdev.com>
2012-06-19 07:48:17 -0500

    fixing csrf token

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,5 +1,6 @@
 class JobsController < ApplicationController
   before_filter :authenticate_user!
+  skip_before_filter :verify_authenticity_token
 
   def index
     @business = Business.find(params[:business_id])
