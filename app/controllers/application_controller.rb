89062accf1f2b9946df249d5ae14b8b4539f46f8
Jonathan Jeffus <jonathan@blazingdev.com>
2012-06-20 11:08:08 -0500

    fixing download and sign up process

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource_or_scope)
     if resource_or_scope.is_a?(User)
-      new_business_path()
+      businesses_path()
     else
       super
     end
