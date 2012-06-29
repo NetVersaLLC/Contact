d9b9855434d3ff5ee2ffa4d5d93238a242899133
Jonathan Jeffus <jonathan@blazingdev.com>
2012-06-28 22:26:23 -0500

    fixed the booboos controller to record message

--- app/controllers/booboos_controller.rb
+++ app/controllers/booboos_controller.rb
@@ -1,9 +1,10 @@
 class BooboosController < InheritedResources::Base
   skip_before_filter :verify_authenticity_token
   def create
-    @booboo             = Booboo.new(params[:booboo])
+    @booboo             = Booboo.new
     @booboo.user_id     = current_user.id if current_user
     @booboo.business_id = params[:business_id]
+    @booboo.message     = params[:message]
     @booboo.user_agent  = request.env['HTTP_USER_AGENT']
     @booboo.ip          = request.remote_ip
     respond_to do |format|
