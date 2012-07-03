c6073bad95aa528a769d1ac3e90680dff8d1c543
Jonathan Jeffus <jonathan@blazingdev.com>
2012-07-02 19:22:49 -0500

    fixing inject job script

--- bin/inject_job.rb
+++ bin/inject_job.rb
@@ -3,17 +3,10 @@
 require 'pp'
 require './config/environment.rb'
 
-user_id     = ARGV.shift
 business_id = ARGV.shift
 payload     = ARGV.shift
 
-u = User.find(user_id)
 b = Business.find(business_id)
-
-if b.user_id != u.id
-  STDERR.puts "User does not own business!"
-  exit
-end
 
 if File.exists? payload
   Job.inject(b.id, File.open(payload, "r").read, 'Ping')
