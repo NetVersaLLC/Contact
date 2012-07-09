068418fd3e28cb4ba19880a31bbeb63acd7ca739
Jonathan Jeffus <jjeffus@gmail.com>
2012-07-09 11:19:46 -0500

    added payload assignment

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,3 +1,4 @@
 class Payload < ActiveRecord::Base
-  attr_accessible :model, :name, :location, :status, :payload
+  attr_accessible :model, :name, :location, :status, :payload, :payload_category_id
+  belongs_to :payload_category
 end
--- db/migrate/20120709152056_create_payload_categories.rb
+++ db/migrate/20120709152056_create_payload_categories.rb
@@ -2,8 +2,10 @@ class CreatePayloadCategories < ActiveRecord::Migration
   def change
     create_table :payload_categories do |t|
       t.string :name
-
+      t.integer :position
       t.timestamps
     end
+    add_column :payloads, :payload_category_id, :integer
+    add_index :payloads, :payload_category_id
   end
 end
