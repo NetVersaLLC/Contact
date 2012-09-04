a0fbb45995000d3972b832c5219eceefe9cd5121
Jonathan Jeffus <jjeffus@gmail.com>
2012-09-04 08:57:50 -0700

    tasks for working with payloads

--- app/models/payload.rb
+++ app/models/payload.rb
@@ -1,4 +1,14 @@
 class Payload < ActiveRecord::Base
   attr_accessible :data_generator, :name, :position, :status, :payload, :payload_category_id
   belongs_to :payload_category
+
+  validates_uniqueness_of :name
+  validates :name,
+    :presence => true
+  validates :position,
+    :presence => true
+  validates :payload,
+    :presence => true
+  validates :payload_category_id,
+    :presence => true
 end
--- app/models/payload_category.rb
+++ app/models/payload_category.rb
@@ -1,4 +1,11 @@
 class PayloadCategory < ActiveRecord::Base
   attr_accessible :name, :position
   has_many :payloads
+  def self.next_position
+    if PayloadCategory.count == 0
+      1
+    else
+      PayloadCategory.order(:position).last.position + 1
+    end
+  end
 end
