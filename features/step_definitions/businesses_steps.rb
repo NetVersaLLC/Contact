d5d33fe8c35465072a66957e1a0de5e42e4e8ee8
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-09-11 14:32:23 +0200

    more checks for: Save without entering data
    Feature: Add business
    Scenario: Save without entering data

--- features/step_definitions/businesses_steps.rb
+++ features/step_definitions/businesses_steps.rb
@@ -2,6 +2,6 @@ When /^I save changes$/ do
   on(HomePage).save_changes
 end
 
-Then /^Business name element should have error (.+)$/ do |error|
-  on(HomePage).business_name_error_element.text.should == error
+Then /^(.+) should have error (.+)$/ do |element, error|
+  on(HomePage).error(element).should == error
 end
--- features/support/pages/home_page.rb
+++ features/support/pages/home_page.rb
@@ -7,10 +7,16 @@ class HomePage
   page_url url
 
   text_field(:business_name, id: "business_business_name")
+  text_field(:local_phone, id: "business_local_phone")
+  text_field(:address, id: "business_address")
+  textarea(:business_description, id: "business_business_description")
+  select(:geographic_areas, id: "business_geographic_areas")
+
   div(:feedback, class: "alert")
   button(:save_changes, name: "commit")
 
-  def business_name_error_element
-    business_name_element.parent
+  def error(element_name)
+    element = "#{element_name}_element".downcase.gsub(" ", "_")
+    send(element).parent.span.text
   end
 end
