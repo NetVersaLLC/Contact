3c45348189dd531491dbedca00b578a33f50d75f
Zeljko Filipin <zeljko.filipin@gmail.com>
2012-09-20 16:39:59 +0200

    add more checks to "Save after entering required data" scenario

--- features/step_definitions/businesses_steps.rb
+++ features/step_definitions/businesses_steps.rb
@@ -2,6 +2,10 @@ When /^I save changes$/ do
   on(HomePage).save_changes
 end
 
+Then /^(.+) should be (.+)$/ do |element_name, value|
+  element = element_name.downcase.gsub(" ", "_")
+  on(HomePage).send(element).should == value
+end
 Then /^(.+) should have error (.+)$/ do |element, error|
   on(HomePage).error(element).should == error
 end
