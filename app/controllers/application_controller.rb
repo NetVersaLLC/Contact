aad79fd03e9b3cd2406eb34c1a68dc70e03fad8a
Jonathan Jeffus <jjeffus@gmail.com>
2012-11-06 19:20:10 -0800

    adding support for white labels

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def current_label
+    label = Label.where(:domain => request.host).first
+    unless label
+      label = Label.first
+    end
+    label
+  end
 end
