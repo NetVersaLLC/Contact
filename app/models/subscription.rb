10b1c486943b98502be0167508bfbcce9767cb8f
Jonathan Jeffus <jjeffus@gmail.com>
2012-11-16 14:14:44 -0800

    fixes to form and data saving

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -3,7 +3,7 @@ class Subscription < ActiveRecord::Base
 
   attr_accessor   :card_number, :card_type, :exp_year, :exp_month, :cvv, :coupon_code
   attr_accessible :card_number, :card_type, :exp_year, :exp_month, :cvv, :coupon_code
-  attr_accessible :address, :address2, :affiliate_id, :city, :first_name, :last_name, :package_id, :package_name, :phone, :state, :tos_agreed, :total, :zip
+  attr_accessible :address,     :address2, :affiliate_id, :city, :first_name, :last_name, :package_id, :package_name, :phone, :state, :tos_agreed, :total, :zip
   has_one :business
   belongs_to :package
 
@@ -12,21 +12,17 @@ class Subscription < ActiveRecord::Base
     :numericality => true
   validates :package_id, :presence => true
   validates :first_name, :presence => true
-  validates :last_name, :presence => true
-  validates :address, :presence => true
-  validates :city, :presence => true
-  validates :state, :presence => true
-  validates :zip, :presence => true
+  validates :last_name,  :presence => true
+  validates :address,    :presence => true
+  validates :city,       :presence => true
+  validates :state,      :presence => true
+  validates :zip,        :presence => true
 
   validates :card_type,   :presence => true
   validates :card_number, :presence => true
   validates :exp_year,    :presence => true
   validates :exp_month,   :presence => true
   validates :cvv,         :presence => true
-
-  validates :coupon_code,
-    :allow_blank => true,
-    :format => {:with => /^[A-Z0-9]{10}$/}
 
   def self.years
     (Time.now.year .. Time.now.year + 10).to_a
