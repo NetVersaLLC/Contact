62e968b21cdc6e3e5b4993dfd83d1aa9d6ff5725
Jonathan Jeffus <jjeffus@gmail.com>
2012-11-16 15:04:20 -0800

    updating scripts to run within the system

--- sites/Getfave/SignUp/data_generator.rb
+++ sites/Getfave/SignUp/data_generator.rb
@@ -1,5 +1,5 @@
 data = {}
-data[ 'email' ] 		= 'netversatest74@gmail.com' #business.yahoos.first.email
-data[ 'password' ] 		= 'Doomsdaydevice1!' #Yahoo.make_password
+data[ 'email' ] 		= business.bings.first.email
+data[ 'password' ] 		= Yahoo.make_password
 data[ 'name' ] 			= business.contact_first_name + ' ' + business.contact_last_name
 data
