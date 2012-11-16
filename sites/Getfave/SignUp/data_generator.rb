50f08ed47c9077804cae6adf747bd7bb8dd41a62
Ubuntu <ubuntu@domU-12-31-39-06-30-E2.compute-1.internal>
2012-11-16 22:08:38 +0000

    joshes scripts

---
+++ sites/Getfave/SignUp/data_generator.rb
@@ -0,0 +1,5 @@
+data = {}
+data[ 'email' ] 		= 'netversatest74@gmail.com' #business.yahoos.first.email
+data[ 'password' ] 		= 'Doomsdaydevice1!' #Yahoo.make_password
+data[ 'name' ] 			= business.contact_first_name + ' ' + business.contact_last_name
+data
