98f9cc4ca4abb1550951c97c36dbd816258d27fa
amitr1983 <amitr1983@github.com>
2012-11-24 00:29:08 +0530

    Changes for sign up & Shared.rb

--- sites/Google/SignUp/client_script.rb
+++ sites/Google/SignUp/client_script.rb
@@ -1,69 +1,88 @@
-def verify_account_name_available( business )
-	# site = 'https://accounts.google.com/SignUp'
-  @name = business['email_name']
-	@browser.text_field(:id, "GmailAddress").set(@name)
-	@browser.send_keys :tab
-	sleep(4)
-	if @browser.span(:id, "errormsg_0_GmailAddress").visible?
-		puts 'Business Name is already used as a google username.  Need alternate!'
-		if business.has_key?('acceptable_alternates')
-			business['acceptable_alternates'].each do |new_name|
-				@browser.text_field(:id, "GmailAddress").set(new_name)
-				@browser.send_keys :tab
-				sleep(4)
-				if not @browser.span(:id, "errormsg_0_GmailAddress").exists?
-          @name = new_name
-					break
-				end
-			end
-		else
-			throw('Business Name is already used as a google username.  Need alternate!')
-		end
-	end
-
-  @name
+def verify_account_name_available( data )
+	
+  #Verify user name is available or not
+  @browser.text_field(:id, "GmailAddress").value = data['email_name']
+  @browser.div(:id,'gmail-address-form-element').span(:text,'@gmail.com').click
+  @browser.text_field(:id, "GmailAddress").send_keys :tab
+  @browser.span(:id, "errormsg_0_GmailAddress").text != ""
+  if @browser.span(:id, "errormsg_0_GmailAddress") != ""
+    puts 'Business Name is already used as a google username.  Need alternate!'
+    if business.has_key?('acceptable_alternates')
+      data['acceptable_alternates'].each do |new_name|
+      @browser.text_field(:id, "GmailAddress").value = new_name
+      @browser.text_field(:id, "GmailAddress").send_keys :tab
+      if not @browser.span(:id, "errormsg_0_GmailAddress").exists?
+        break
+      end
+      end
+    else
+      fail StandarError.new('Business Name is already used as a google username.  Need alternate!')
+    end
+  end
 end
 
-def signup_generic( business )
-  @browser.goto 'https://accounts.google.com/SignUp'
-	
-	@browser.text_field(:id, "FirstName").set(business['first name'])
-	@browser.text_field(:id, "LastName").set(business['last name'])
-	
-	@account_name = verify_account_name_available( business )
-	
-	@browser.text_field(:id, "Passwd").set business['password']
-	@browser.text_field(:id, "PasswdAgain").set business['password']
-	
-	@browser.send_keys :tab
-	# Number of times to select month
-	8.times { @browser.send_keys :arrow_down; }
-	
-	@browser.send_keys :tab
-	@browser.text_field(:id, "BirthDay").set business['birthday']
-	@browser.text_field(:id, "BirthYear").set business['birthyear']
-	
-	@browser.send_keys :tab
-	# Gender
-	3.times { @browser.send_keys :arrow_down; }
-	
-	#@browser.text_field(:id, "RecoveryPhoneNumber").set business['phone_number']
-	#@browser.text_field(:id, "RecoveryEmailAddress").set business['email']
+def signup_generic( data )
 
-	# TODO Captcha
-	#<img class="decoded" alt="https://www.google.com/recaptcha/api/image?c=03AHJ_VushQ-GsHbv6xNOVQfzKE8jfDFrqBB1GSvfA55jaI2PNtpVGuM1Eo2jSjd0DSezOCDSvvDKojdrEv020d_5j9Tgv0eNLRn0KEJ8rJ4c724UmGb1oH4kLMoYx9mMPdstYXdtnaFAanpTkgJS1PBQcSVY1ZhqCRg" src="https://www.google.com/recaptcha/api/image?c=03AHJ_VushQ-GsHbv6xNOVQfzKE8jfDFrqBB1GSvfA55jaI2PNtpVGuM1Eo2jSjd0DSezOCDSvvDKojdrEv020d_5j9Tgv0eNLRn0KEJ8rJ4c724UmGb1oH4kLMoYx9mMPdstYXdtnaFAanpTkgJS1PBQcSVY1ZhqCRg">
+  site = 'https://accounts.google.com/SignUp'
+	
+  #Launch Browser
+  @browser.goto site
+
+  @browser.text_field(:id, "FirstName").value = data['first name']
+  @browser.text_field(:id, "LastName").value = data['last name']
+	
+  #Verify if username is available
+  verify_account_name_available( business )
+	
+  @browser.text_field(:id, "Passwd").value = data['pass']
+  @browser.text_field(:id, "PasswdAgain").value = data['pass']
+
+  # Number of times to select month
+  @browser.div(:class => "goog-inline-block goog-flat-menu-button-dropdown", :index => 0).click
+  @browser.div(:text, "#{data['birthmonth']}").click
+  @browser.text_field(:id, "BirthDay").value = data['birthday']
+  @browser.text_field(:id, "BirthYear").value = data['birthyear']
+
+  # Gender
+  @browser.div(:class => "goog-inline-block goog-flat-menu-button-dropdown", :index => 1).click
+  @browser.div(:text, "#{data['gender']}").click
+  @browser.text_field(:id, "RecoveryPhoneNumber").set data['phone']
+  #~ @browser.text_field(:id, "RecoveryEmailAddress").value = data['alt_email']
+	
+  @browser.div(:class => "goog-inline-block goog-flat-menu-button-dropdown", :index => 2).click
+  @browser.div(:text, "#{data['country']}").click
+  @browser.checkbox(:id,'TermsOfService').set
+	
+  #Captcha Code
   file = Tempfile.new('image.png')
   file.close
+  
+
   @browser.image(:src, /recaptcha\/api\/image/).save file.path
   text = CAPTCHA.solve file.path
   @browser.text_field( :id => 'captcha_text' ).set text
 	
-	$browser.button(:id, "submitbutton").focus
-	$browser.button(:id, "submitbutton").send_keys :return
+  @browser.button(:value, 'Next step').click
+  @browser.wait
+	
 
-  RestClient.post "#{@host}/google/save_email?auth_token=#{@key}&business_id=#{@bid}", :email => "#{@name}@gmail.com", :password => business['password']
+  if @browser.text.include?('Verify your account')
+    @browser.text_field(:id, 'signupidvinput').set data[ 'phone' ]
+    @browser.radio(:id,'signupidvmethod-sms').set
+    @browser.button(:value,'Continue').click
+    if @browser.span(:class,'errormsg').exist? 
+      puts "#{@browser.span(:class,'errormsg').text}"
+    end
+    if @browser.text_field(:id,'verify-phone-input').exist?
+      @browser.text_field(:id,'verify-phone-input').set data[ 'pass-code' ]
+      @browser.button(:value,'Continue').click
+        if @browser.span(:class,'errormsg').exist? 
+    	puts "#{@browser.span(:class,'errormsg').text}"
+        end
+    end
+  else
+    throw("Initial Registration is not successful")
+  end	
 end
 
 signup_generic(data)
-
-true
--- sites/Google/SignUp/data_generator.rb
+++ sites/Google/SignUp/data_generator.rb
@@ -1,14 +1,18 @@
 email_names = Google.make_email_names(business)
 
-data = { # 'email' => '',
-         # 'phone_number' => business.local_phone,
+data = { 'phone' => business.local_phone,
          'pass' => Google.make_password,
-         'first name' => business.contact_first_name,
-         'last name' => business.contact_last_name,
-         'email_name' => email_names.shift,
+         'first_name' => business.contact_first_name,
+         'last_name' => business.contact_last_name,
+         #~ 'email_name' => email_names.shift, 
+	 'email_name' => business.googles.first.email,
          'acceptable_alternates' => email_names,
          'address' => business.address + ' ' + business.address2,
          'zipcode' => business.zip,
-         'country' => 'United States'
+         'country' => 'United States',
+	 'month' => Date::MONTHNAMES[business.birthday.month],
+	 'day' => business.birthday.day.to_s,
+	 'year' => business.birthday.year.to_s,
+	 'gender' => business.contact_gender
 }
 data
