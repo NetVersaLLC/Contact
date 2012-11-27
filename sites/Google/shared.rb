5072484aa7c34d5cc4d974cdd1679ad4cc78073d
amitr1983 <amitr1983@github.com>
2012-11-27 23:56:50 +0530

    Seperating out code for create & claim listing

--- sites/Google/shared.rb
+++ sites/Google/shared.rb
@@ -25,3 +25,35 @@ def login ( data )
     raise StandardError.new("You must provide both a username AND password for gplus_login!")
   end
 end
+
+def search_for_business( data )
+
+	puts 'Search for the ' + data[ 'business' ] + ' business at ' + data[ 'zip' ] +  data['zip_location']
+	# 'https://plus.google.com/local' ) # Must be logged in to search
+	@browser.goto('https://plus.google.com/local')
+
+	@browser.text_field(:name, "qc").set data['business']
+	@browser.text_field(:name, "qb").set data['zip_location']
+	@browser.button(:id,'gbqfb').click
+	@browser.wait
+	sleep(5)
+end
+
+def parse_results( data )
+#Parse search result page
+	page = Nokogiri::HTML(@browser.html)
+	page_links = page.css("a").select
+	applicableLinks = {}
+	page_links.each do |link|
+		if not link.nil?
+			if not link['href'].nil? and !!link['href']["/about"]
+				img = ""
+				if not link.at('img').nil?
+					img = link.at('img')['src']
+				end
+				applicableLinks[link.content] = [link['href'], img]
+			end
+		end
+	end
+	return applicableLinks.to_a
+end
