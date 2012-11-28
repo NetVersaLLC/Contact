47a08fa998101022cbbdc5fd0c3c053b6591a42f
JoshWillis <blue74dot@gmail.com>
2012-11-28 22:00:54 +0000

    Merchantcircle bans IPs from registering, after a successful registration, for 20~. This works in theory, but testing it is a slow process given my limited supply of IPs.

--- app/models/kudzu.rb
+++ app/models/kudzu.rb
@@ -5,4 +5,17 @@ class Kudzu < ClientData
             :presence => true
   validates :password,
             :presence => true
+
+  def self.check_email(business)
+    @link = nil
+    CheckMail.get_link(business) do |mail|
+      if mail.subject =~ /Please confirm your Kudzu profile/
+        if mail.body =~ /(https:\/\/register.kudzu.com\/confirmEmail.do?confirmCode=\S+)/
+          @link = $1
+        end
+      end
+    end
+    @link
+  end
+
 end
