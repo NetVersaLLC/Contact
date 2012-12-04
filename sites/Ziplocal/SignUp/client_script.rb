057aa62039be6de189a9b072536a6a611f33f86e
JoshWillis <blue74dot@gmail.com>
2012-12-04 23:37:20 +0000

    Adding the working ZipLocal

--- sites/Ziplocal/SignUp/client_script.rb
+++ sites/Ziplocal/SignUp/client_script.rb
@@ -11,8 +11,8 @@ def add_business( data )
 	@browser.text_field( :id, 'listingForm:zip').set data[ 'zip' ]
 	@browser.text_field( :id, 'listingForm:web').set data[ 'website' ]
 	@browser.text_field( :id, 'listingForm:email').set data[ 'business_email' ]
-	@browser.text_field( :id, 'listingForm:_id135').set data[ 'first_name' ] + ' ' + data[ 'last_name' ] 
-	@browser.text_field( :id, 'listingForm:_id140').set data[ 'email' ]
+	@browser.text_field( :xpath, '/html/body[2]/form/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div[3]/div/div/table/tbody/tr/td[2]/input').set data[ 'first_name' ] + ' ' + data[ 'last_name' ] 
+	@browser.text_field( :xpath, '/html/body[2]/form/table/tbody/tr[2]/td/table/tbody/tr[2]/td/div[3]/div/div/table/tbody/tr[2]/td[2]/input').set data[ 'email' ]
 	@browser.radio( :value, 'O').set
 
 	#Now to add categories. At least 1 category is required.
@@ -30,10 +30,10 @@ def add_business( data )
 	@browser.button( :value, 'Add new').flash
 	@browser.button( :value, 'Add new').click
 	sleep(3)
-	@browser.link( :id, 'listingForm:_id146').click
+	@browser.link( :text, 'Save').click
 	
 	if @browser.text.include? 'Thank you for updating our directory. Updates take effect within 3 business days.'
-		puts ('Business added successfully')
+		puts('Business added successfully')
 	end
 
 
@@ -45,7 +45,7 @@ end
 
 def main( data )
 #this method runs the show
-	@browser.goto  ('http://www.ziplocal.com/olc/lookup.faces')
+	@browser.goto('http://www.ziplocal.com/olc/lookup.faces')
 
 	#wait for page to load
 	Watir::Wait::until do @browser.text.include? 'Enter 10 digit phone number' end
@@ -54,7 +54,7 @@ def main( data )
 	@browser.text_field( :id, 'lookupForm:tel').set data[ 'phone' ]
 	@browser.link( :id, 'lookupForm:submit').click
 	
-	#@browser.goto  ('')
+	#@browser.goto('')
 
 	#wait until the next page loads
 	Watir::Wait::until do @browser.text.include? 'We did not find the listings for phone number' or
