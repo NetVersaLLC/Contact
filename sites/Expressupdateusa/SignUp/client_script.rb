e5d7c28672d664d9a2b3e1f5752151a134a36484
JoshWillis <blue74dot@gmail.com>
2012-12-07 16:23:03 +0000

    Adding expressupdateusa.

---
+++ sites/Expressupdateusa/SignUp/client_script.rb
@@ -0,0 +1,29 @@
+
+
+def sign_up( data )
+
+  # assert url starts with 'https://listings.expressupdateusa.com/Account/Register'
+  puts 'Signing up with email: ' + data[ 'personal_email' ]
+
+  @browser.text_field( :id, 'Email' ).set data[ 'personal_email' ]
+  @browser.text_field( :id, 'Password' ).set data[ 'personal_password' ]
+  @browser.text_field( :id, 'ConfirmPassword' ).set data[ 'personal_password' ]
+
+  @browser.text_field( :id, 'Phone' ).set data[ 'business_phone' ]
+  @browser.text_field( :id, 'BusinessName' ).set data[ 'business_name' ]
+  @browser.text_field( :id, 'FirstName' ).set data[ 'personal_firstname' ]
+  @browser.text_field( :id, 'LastName' ).set data[ 'personal_lastname' ]
+  
+  @browser.select_list( :id, 'State' ).select data[ 'business_state' ]
+  @browser.text_field( :id, 'Captcha').set solve_captcha
+  @browser.checkbox( :id, 'DoesAcceptTerms' ).set
+  @browser.button( :class, 'RegisterNowButton' ).click
+
+  # If no return URl then 'Thank You for Registering with Express Update. An activation email sent!'
+
+end
+
+
+@browser.goto('https://listings.expressupdateusa.com/Account/Register')
+sign_up( data )
+
