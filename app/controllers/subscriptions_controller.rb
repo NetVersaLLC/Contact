085111321546721e5e66ab2b4b50b9c1f7802274
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-10 16:30:21 -0800

    adding subscription fixes for subscription management

--- app/controllers/subscriptions_controller.rb
+++ app/controllers/subscriptions_controller.rb
@@ -6,6 +6,14 @@ class SubscriptionsController < ApplicationController
     @subscription = Subscription.new
   end
 
+  def edit
+    @business = Business.find(params[:id])
+    @subscription = @business.subscription
+    if @subscription == nil
+      @subscription = Subscription.new
+    end
+  end
+
   def create
     sub           = params['subscription']
     if sub['coupon_code'] == 'NETVERSA'
@@ -18,10 +26,14 @@ class SubscriptionsController < ApplicationController
         s.active       = true
       end
       @subscription.save
-      business                 = Business.new
+      if params[:business_id]
+        business = Business.find(params[:business_id])
+      else
+        business = Business.new
+      end
       business.user_id         = current_user.id
       business.subscription_id = @subscription.id
-      business.save     :validate => false
+      business.save              :validate => false
       redirect_to edit_business_path(business)
       return
     end
@@ -38,7 +50,6 @@ class SubscriptionsController < ApplicationController
     @subscription.zip        = sub['zip']
     @subscription.tos_agreed = true
     @subscription.active     = true
-    @subscription.authorizenet_code = response.status
 
     # Copy values over to re-render the form in case of
     # an error.
@@ -58,7 +69,7 @@ class SubscriptionsController < ApplicationController
       :last_name          => sub['last_name']
     )
     if credit_card.valid?
-      response = ::AUTHORIZENETGATEWAY.recurring(package.price, credit_card, {
+      resp = ::AUTHORIZENETGATEWAY.recurring(package.price, credit_card, {
         :interval => {
           :unit => :months,
           :length => 1
@@ -76,8 +87,11 @@ class SubscriptionsController < ApplicationController
           :country    => 'United States',
           :zip        => sub['zip']
       }})
-      if response.success?
+      if resp.success?
+        logger.info "Printing response:"
+        logger.info resp.to_json
         flash[:notice] = "Purchase complete!"
+        @subscription.authorizenet_code = resp.authorization
         @subscription.save
         business                 = Business.new
         business.user_id         = current_user.id
@@ -85,7 +99,102 @@ class SubscriptionsController < ApplicationController
         business.save     :validate => false
         redirect_to edit_business_path(business)
       else
-        flash[:alert] = "Error: #{response.message}"
+        flash[:alert] = "Error: #{resp.message}"
+        render :action => 'new'
+      end
+    else
+      flash[:alert] = "Error: Credit card is invalid."
+      render :action => 'new'
+    end
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    sub       = params['subscription']
+    if sub['coupon_code'] == 'NETVERSA'
+      flash[:notice]   = "Purchase complete!"
+      @subscription    = Subscription.create do |s|
+        s.package_id   = Package.first
+        s.package_name = Package.first.name
+        s.total        = Package.first.price
+        s.tos_agreed   = true
+        s.active       = true
+      end
+      @subscription.save
+      if params[:business_id]
+        business = Business.find(params[:business_id])
+      else
+        business = Business.new
+      end
+      business.user_id         = current_user.id
+      business.subscription_id = @subscription.id
+      business.save              :validate => false
+      redirect_to edit_business_path(business)
+      return
+    end
+    package       = Package.find( sub['package_id'] )
+    @subscription = Subscription.new
+    @subscription.package_id = package.id
+    @subscription.total      = package.price
+    @subscription.first_name = sub['first_name']
+    @subscription.last_name  = sub['last_name']
+    @subscription.address    = sub['address']
+    @subscription.address2   = sub['address2']
+    @subscription.city       = sub['city']
+    @subscription.state      = sub['state']
+    @subscription.zip        = sub['zip']
+    @subscription.tos_agreed = true
+    @subscription.active     = true
+
+    # Copy values over to re-render the form in case of
+    # an error.
+    @subscription.card_type   = sub['card_type']
+    @subscription.card_number = sub['card_number']
+    @subscription.cvv         = sub['cvv']
+    @subscription.exp_month   = sub['exp_month']
+    @subscription.exp_year    = sub['exp_year']
+
+    credit_card = ActiveMerchant::Billing::CreditCard.new(
+      :type               => sub['card_type'],
+      :number             => sub['card_number'],
+      :verification_value => sub['cvv'],
+      :month              => sub['exp_month'],
+      :year               => sub['exp_year'],
+      :first_name         => sub['first_name'],
+      :last_name          => sub['last_name']
+    )
+    if credit_card.valid?
+      resp = ::AUTHORIZENETGATEWAY.recurring(package.price, credit_card, {
+        :interval => {
+          :unit => :months,
+          :length => 1
+        },
+        :duration => {
+          :start_date => Date.today,
+          :occurrences => 9999
+        },
+        :billing_address => {
+          :first_name => sub['first_name'],
+          :last_name  => sub['last_name'],
+          :address1   => sub['address'] + ' ' + sub['address2'],
+          :city       => sub['city'],
+          :state      => sub['state'],
+          :country    => 'United States',
+          :zip        => sub['zip']
+      }})
+      if resp.success?
+        logger.info "Printing response:"
+        logger.info resp.to_json
+        flash[:notice] = "Purchase complete!"
+        @subscription.authorizenet_code = resp.authorization
+        @subscription.save
+        business                 = @business
+        business.user_id         = current_user.id
+        business.subscription_id = @subscription.id
+        business.save     :validate => false
+        redirect_to edit_business_path(business)
+      else
+        flash[:alert] = "Error: #{resp.message}"
         render :action => 'new'
       end
     else
@@ -95,6 +204,19 @@ class SubscriptionsController < ApplicationController
   end
 
   def destroy
-    @sub = Subscription.find(params[:id])
+    @sub = Business.find(params[:id]).subscription
+    if @sub.active == false
+      flash[:alert] = "Subscription already cancelled."
+    else
+      resp = ::AUTHORIZENETGATEWAY.cancel_recurring(@sub.authorizenet_code)
+      if resp.success?
+        @sub.active = false
+        @sub.save!
+        flash[:alert] = "Subscription cancelled."
+      else
+        flash[:alert] = "Error: #{resp.message}"
+      end
+    end
+    redirect_to businesses_path
   end
 end
