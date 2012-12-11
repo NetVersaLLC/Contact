60edd9b09016f4076b4136e7815ec8d808880468
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-11 15:54:48 -0800

    fixing the production env

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -4,12 +4,13 @@
 # Password: ErK448qcKc 
 
 if Rails.env == "production"
+  ActiveMerchant::Billing::Base.mode = :production
   ::AUTHORIZENETGATEWAY = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
     :login    => "5SJ7zUf5m2c3",
     :password => "3DEM54zqk68BbZ34",
     :test     => false
   )
-  ::AUTHORIZENETGATEWAY.arb_live_url = 'https://secure.authorize.net/gateway/transact.dll'
+  ::AUTHORIZENETGATEWAY.arb_live_url = 'https://api.authorize.net/xml/v1/request.api'
 else
   ::AUTHORIZENETGATEWAY = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
     :login    => "8g5FEvUR7De",
