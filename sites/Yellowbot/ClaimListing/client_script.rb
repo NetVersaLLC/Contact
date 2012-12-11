5a30924f8e044364bcae20f576b10deb3b090677
JoshWillis <blue74dot@gmail.com>
2012-12-11 17:26:07 +0000

    Adding phone verification to Yellowbot. Needs testing with a real business as Yellowbot uses the phone number it has on record, not the one you enter.

--- sites/Yellowbot/ClaimListing/client_script.rb
+++ sites/Yellowbot/ClaimListing/client_script.rb
@@ -2,8 +2,9 @@
 @browser.text_field( :id => 'search-field' ).set data[ 'phone' ]
 @browser.button( :value => 'Find my business' ).click #, :type => 'submit'
 
-def submit_link; @browser.link( :text => 'submit a new business') end
-def claim_link; @browser.link( :text => 'Claim' ) end
+@browser.link( :text, "#{data[ 'name' ]}").click
+@browser.link( :text => 'Claim it now!' ).click
 
-#TODO ADD PHONE VERIFICATION
-#@browser.link( :text, 'Claim my business').click
+@browser.link( :text, 'Claim my business').click
+
+code = PhoneVerify.ask_for_code(number)
