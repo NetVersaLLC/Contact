dc91900ed563f52005357c032cc8bf613bef0e90
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-11 14:24:46 -0800

    adding support for real transactions

--- config/initializers/active_merchant.rb
+++ config/initializers/active_merchant.rb
@@ -3,9 +3,18 @@
 # Username: netversa411
 # Password: ErK448qcKc 
 
-::AUTHORIZENETGATEWAY = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
-  :login    => "8g5FEvUR7De",
-  :password => "3DYc4hBQ4gN856xv",
-  :test     => true
-)
-::AUTHORIZENETGATEWAY.arb_live_url = 'https://apitest.authorize.net/xml/v1/request.api'
+if Rails.env == "production"
+  ::AUTHORIZENETGATEWAY = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
+    :login    => "5SJ7zUf5m2c3",
+    :password => "3DEM54zqk68BbZ34",
+    :test     => false
+  )
+  ::AUTHORIZENETGATEWAY.arb_live_url = 'https://secure.authorize.net/gateway/transact.dll'
+else
+  ::AUTHORIZENETGATEWAY = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
+    :login    => "8g5FEvUR7De",
+    :password => "3DYc4hBQ4gN856xv",
+    :test     => true
+  )
+  ::AUTHORIZENETGATEWAY.arb_live_url = 'https://apitest.authorize.net/xml/v1/request.api'
+end
