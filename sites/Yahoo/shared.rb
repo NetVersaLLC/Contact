4f1d4498d90242541a442f54d37c35c968931a03
amitr1983 <amitr1983@github.com>
2012-12-14 00:37:48 +0530

    Added 5 times retry for captcha

--- sites/Yahoo/shared.rb
+++ sites/Yahoo/shared.rb
@@ -5,17 +5,23 @@ def sign_in(business)
   @browser.button(:id, '.save').click
 end
 
-def retry_captcha
+def retry_captcha(captcha_text)
    @captcha_error = @browser.div(:id => 'captchaFld')
    @captcha_error_msg = "Please try this code instead"
-   #Check if there is any captcha mismatch
-   if @captcha_error.exist? && @captcha_error.text.include?(@captcha_error_msg)
+   count = 1
+# Decode captcha code until its decoded (Maximum 5 times)
+   while @captcha_error.exist? do
+      @browser.text_field( :id => 'captchaV5Answer' ).set captcha_text
+      @browser.button( :id => 'IAgreeBtn' ).click
+      count+=1
+      break if count == 5
+   end
+end
+
+def solve_captcha
       file = "#{ENV['USERPROFILE']}\\citation\\yahoo_captcha.png"
       @browser.image(:class, 'captchaImage').save file
       text = CAPTCHA.solve file, :manual
-      @browser.text_field( :id => 'captchaV5Answer' ).set text
-      sleep 12
-      @browser.button( :id => 'IAgreeBtn' ).click
-   end
+  return text
 end
 
