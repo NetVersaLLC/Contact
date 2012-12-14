54081dd8a92fc08742a14d9e395e197961091279
JoshWillis <blue74dot@gmail.com>
2012-12-14 18:07:22 +0000

    Adding email verification to Tupalo

--- app/models/tupalo.rb
+++ app/models/tupalo.rb
@@ -2,4 +2,21 @@ class Tupalo < ClientData
   attr_accessible :username
   virtual_attr_accessor :password
 
+
+  def self.check_email(business)
+    @password = nil
+    CheckMail.get_link(business) do |mail|
+      if mail.subject =~ /Welcome to Tupalo.com?/i
+        mail.parts.map do |p|
+          if p.content_type =~ /text\/html/
+		@password = p.decoded.scan(/<b>Password:<\/b> (.*)<br\/>/)            
+          end
+        end
+      end
+    end
+    STDERR.puts "Tupalo password: #{@password}"
+    @password
+  end
+
+
 end
