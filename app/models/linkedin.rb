4d0c8e935bd0606f6b9ac62ee895deb2c7ecf7da
JoshWillis <blue74dot@gmail.com>
2012-12-14 20:54:21 +0000

    Adding Linkedin

--- app/models/linkedin.rb
+++ app/models/linkedin.rb
@@ -6,4 +6,24 @@ class Linkedin < ClientData
             :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
   validates :password,
             :presence => true
+
+
+  def self.check_email(business)
+    @link = nil
+    CheckMail.get_link(business) do |mail|
+      if mail.subject =~ /Please confirm your email address/i
+        mail.parts.map do |p|
+          if p.content_type =~ /text\/html/
+            nok = Nokogiri::HTML(p.decoded)
+            @link = nok.xpath("//a")[0].attr('href')
+          end
+        end
+      end
+    end
+    STDERR.puts "Linkedin link: #{@link}"
+    @link
+  end
+
+
+
 end
---
+++ sites/Linkedin/SignUp/client_script.rb
@@ -0,0 +1,11 @@
+goto_signup_page
+process_linkedin_signup(data)
+
+RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[email]' => data['email'], 'account[password]' => data['password'], 'model' => 'Linkedin'
+
+sleep(3)
+
+@browser.link( :text, /Send a confirmation email instead/).click
+
+true
+
