6ff0eb85efe61542ef98ad22920b864fe14df146
JoshWillis <blue74dot@gmail.com>
2012-12-17 22:50:59 +0000

    Adding Insiderpages.

--- app/models/insider_page.rb
+++ app/models/insider_page.rb
@@ -6,4 +6,19 @@ class InsiderPage < ClientData
             :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }
   validates :password,
             :presence => true
+
+  def self.check_email(business)
+    @link = nil
+    CheckMail.get_link(business) do |mail|
+      puts(mail.subject)
+      STDERR.puts mail.subject
+      if mail.subject =~ /Please confirm you Insider Pages account email address/i
+        if mail.body.decoded =~ /(http:\/\/www.insiderpages.com\/confirm\/\S+)/i
+          @link = $1
+        end
+      end
+    end
+    @link
+  end
+
 end
