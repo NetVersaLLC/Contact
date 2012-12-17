a390175e4736ea86faf4e852f1c6b9f06f994e60
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-17 08:41:22 -0800

    adding cancan for admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,12 @@ class ApplicationController < ActionController::Base
     end
     label
   end
+
+  rescue_from CanCan::AccessDenied do |exception|
+    redirect_to root_url, :alert => exception.message
+  end
+
+  def current_ability
+    @current_ability ||= Ability.new(current_admin_user)
+  end 
 end
