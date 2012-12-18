9df4dcaeb41c0e07c28d9f87f2852697f053656f
JoshWillis <blue74dot@gmail.com>
2012-12-18 21:37:07 +0000

    Adding Ibegin, now with 87% more workingness.

--- sites/Ibegin/CreateListing/data_generator.rb
+++ sites/Ibegin/CreateListing/data_generator.rb
@@ -1,5 +1,5 @@
 data = {}
-data[ 'email' ]			= business.bings.first.email
+data[ 'email' ]			= business.ibegins.first.email
 data[ 'password' ]		= business.ibegins.first.password
 data[ 'business_name' ]		= business.business_name
 data[ 'country' ]		= "United States"
@@ -9,14 +9,16 @@ data[ 'address' ]		= business.address + ' ' + business.address2
 data[ 'zip' ]			= business.zip
 data[ 'phone' ]			= business.local_phone
 data[ 'fax' ]			= business.fax_number
-data[ 'category1' ]		= business.category1
-data[ 'category2' ]		= business.category2
-data[ 'category3' ]		= business.category3
+data[ 'category1' ]		= "Restaurants"#business.category1
+data[ 'category2' ]		= "Scuba"#business.category2
+data[ 'category3' ]		= "Orchard"#business.category3
 data[ 'url' ]			= business.company_website
-data[ 'facebook' ]		= business.facebooks.first.email
-data[ 'twitter_name' ]		= business.twitters.first.username
+data[ 'facebook' ]		= ""#business.facebooks.first.email
+data[ 'twitter_name' ]		= ""#business.twitters.first.username
 data[ 'desc' ]			= business.business_description
 data[ 'brands' ]		= ""#
 data[ 'products' ]		= ""#
-data[ 'services' ]		= ""#
+data[ 'services' ]		= business.keyword1 + ', ' +business.keyword2 + ', ' +business.keyword3 + ', ' +business.keyword4 + ', ' +business.keyword5
+data[ 'payment_methods' ]	= Ibegin.payment_methods( business )
 data
+
