a4f517b52e9eeee512e6fd9b01536831d21f2e98
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-18 12:30:15 -0800

    hopefully this will fix the ability issue

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,7 +4,7 @@ class Ability
   def initialize(user)
     if user and user.admin?
       can :manage, :all
-    elsif user
+    elsif user and user.class.to_s == 'AdminUser'
       can :manage, Business
       can :manage, Package
       can :manage, Subscription
