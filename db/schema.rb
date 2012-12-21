839a770fb84da6b4d1aea1d7be221f240c0ad1ce
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-21 15:22:38 -0800

    fixes to deploy to staging

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121220174400) do
+ActiveRecord::Schema.define(:version => 20121220193520) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
@@ -463,15 +463,6 @@ ActiveRecord::Schema.define(:version => 20121220174400) do
 
   add_index "insider_pages", ["business_id"], :name => "index_insider_pages_on_business_id"
 
-  create_table "insiderpages", :force => true do |t|
-    t.integer  "business_id"
-    t.string   "email"
-    t.text     "secrets"
-    t.datetime "force_update"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
-  end
-
   create_table "jobs", :force => true do |t|
     t.integer  "business_id"
     t.string   "name"
@@ -741,14 +732,13 @@ ActiveRecord::Schema.define(:version => 20121220174400) do
   add_index "superpages", ["business_id"], :name => "index_superpages_on_business_id"
 
   create_table "tasks", :force => true do |t|
-    t.integer  "user_id"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.integer  "business_id"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+    t.datetime "started_at"
   end
 
-  add_index "tasks", ["name"], :name => "index_tasks_on_name"
-  add_index "tasks", ["user_id"], :name => "index_tasks_on_user_id"
+  add_index "tasks", ["business_id"], :name => "index_tasks_on_user_id"
 
   create_table "thumbtacks", :force => true do |t|
     t.integer  "business_id"
