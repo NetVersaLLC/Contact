bb8fe6d12e938bfe8e52936927c609f93c71cc7d
Jonathan Jeffus <jjeffus@gmail.com>
2012-12-21 15:22:38 -0800

    fixes to deploy to staging

--- app/models/yellowise.rb
+++ app/models/yellowise.rb
@@ -1,7 +1,7 @@
 class Yellowise  < ClientData
-	attr_accessible :username
-	virtual_attr_accessor :password
-	validates :password,
+  attr_accessible :username
+  virtual_attr_accessor :password
+  validates :password,
             :presence => true
 
 
@@ -9,8 +9,8 @@ class Yellowise  < ClientData
     @link = nil
     CheckMail.get_link(business) do |mail|
       if mail.subject =~ /LocalXML Business Claiming Sign Up/i
-		nok = Nokogiri::HTML(mail.body.decoded)
-		@link = nok.xpath("//a")[1].inner_html #attr('href')
+        nok   = Nokogiri::HTML(mail.body.decoded)
+        @link = nok.xpath("//a")[1].inner_html #attr('href')
       end
     end
     @link
--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,14 @@ set :rvm_type, :user
 # set :rvm_type, :system
 # set :rvm_bin_path, "/home/deploy/.rvm/bin"
 
+set :user, 'deploy'
+set :deploy_to, '/home/deploy/contact'
+set :keep_releases, 5
+server "staging.netversa.com", :app, :web, :db, :primary => true
+set :default_shell, "bash -l"
+set :rvm_ruby_string, '1.9.3'
+set :rvm_type, :user
+
 set :application, 'contact'
 set :scm        , :git
 set :repository , 'git@github.com:jjeffus/Contact.git'
