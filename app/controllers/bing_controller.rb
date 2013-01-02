c1f15f727c56054e45a758816ff086a515afcfc5
Jonathan Jeffus <jjeffus@gmail.com>
2013-01-02 13:22:44 -0800

    fixing home page to display custom layout

--- app/controllers/bing_controller.rb
+++ app/controllers/bing_controller.rb
@@ -1,19 +1,6 @@
 class BingController < ApplicationController
   skip_before_filter :verify_authenticity_token
   respond_to    :js
-  def save_hotmail
-    @business = Business.find(params[:business_id])
-    if current_user.nil? or @business.user_id != current_user.id
-      redirect_to '/', :status => 403
-    else
-      bing               = @business.bings.first
-      bing.email         = params[:email]
-      bing.password      = params[:password]
-      bing.secret_answer = params[:secret_answer]
-      bing.save
-    end
-    render json: {'status' => 'success'}
-  end
 
   def bing_category
     render json: BingCategory.categories
