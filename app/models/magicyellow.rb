8b1f5fff087fee79b38f3c7f5edd680dcc74f6ec
amitr1983 <amitr1983@github.com>
2013-01-08 01:00:34 +0530

    Minor:fix MagicYellow

--- app/models/magicyellow.rb
+++ app/models/magicyellow.rb
@@ -1,3 +1,6 @@
 class Magicyellow < ClientData
   attr_accessible :business_id, :created_at, :email, :force_update, :updated_at
+  virtual_attr_accessor :password
+  validates :password,
+	:presence => true
 end
