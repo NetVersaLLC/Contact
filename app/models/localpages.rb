e239775a1666883abae113f86fa7590b5e356ea9
JoshWillis <blue74dot@gmail.com>
2013-01-07 22:01:44 +0000

    Adding Localpages

---
+++ app/models/localpages.rb
@@ -0,0 +1,19 @@
+class Localpages < ClientData
+	attr_accessible :username
+	virtual_attr_accessor :password
+	validates :password,
+            :presence => true
+
+  def self.check_email(business)
+    @password = nil
+    CheckMail.get_link(business) do |mail|
+  	if mail.subject =~ /Thank you for registering on localpages.com/i
+		if mail.body.decoded =~ /Password: (.*\S+)<br><br>/i
+	          @password = $1
+	        end
+  	end
+    end
+    @password
+  end
+
+end
