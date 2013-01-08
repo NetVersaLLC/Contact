021baeeb50402ce2b0ee0eca28ca9a51bddd2f84
JoshWillis <blue74dot@gmail.com>
2013-01-08 18:31:15 +0000

    Actually adding Gomylocal

---
+++ sites/Gomylocal/shared.rb
@@ -0,0 +1,34 @@
+def solve_captcha
+  image = "#{ENV['USERPROFILE']}\\citation\\gomylocal_captcha.png"
+  obj = @browser.img( :xpath, '//*[@id="captchaimg"]' )
+  puts "CAPTCHA source: #{obj.src}"
+  puts "CAPTCHA width: #{obj.width}"
+  obj.save image
+
+  CAPTCHA.solve image, :manual
+end
+
+def enter_captcha( data )
+
+	capSolved = false
+	count = 1
+	until capSolved or count > 5 do
+		captcha_code = solve_captcha	
+		@browser.text_field( :id, '6_letters_code').set captcha_code	
+		@browser.image( :src => '/images/submit_button.jpg').click
+		sleep(2)
+		if not @browser.alert.exists?
+			capSolved = true
+		else
+			@browser.alert.ok
+		end		
+	count+=1
+	end
+	if capSolved == true
+		true
+	else
+		throw("Captcha was not solved")
+	end
+end
+
+
