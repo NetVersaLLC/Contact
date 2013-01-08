1d0aa47b239a9c4579ec0d1f41d382af9961fc8b
Jonathan Jeffus <jjeffus@gmail.com>
2013-01-08 12:41:20 -0800

    removing cancan to fix things

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,11 +9,11 @@ class ApplicationController < ActionController::Base
     label
   end
 
-  rescue_from CanCan::AccessDenied do |exception|
-    redirect_to root_url, :alert => exception.message
-  end
+  # rescue_from CanCan::AccessDenied do |exception|
+  #   redirect_to root_url, :alert => exception.message
+  # end
 
-  def current_ability
-    @current_ability ||= Ability.new(current_admin_user)
-  end 
+  # def current_ability
+  #   @current_ability ||= Ability.new(current_admin_user)
+  # end 
 end
