de4bba37d4585efd3437eaee56a65b20f108ea72
JoshWillis <blue74dot@gmail.com>
2013-01-17 01:05:19 +0000

    Adding Ezlocal category thingies

---
+++ app/models/ezlocal_category.rb
@@ -0,0 +1,7 @@
+class EzlocalCategory < SiteCategory
+	attr_accessible :name, :parent_id  
+	acts_as_tree :order => :name
+	belongs_to :google_category
+	has_many :ezlocal
+  
+end
---
+++ db/migrate/20130116224809_create_ezlocals.rb
@@ -0,0 +1,11 @@
+class CreateEzlocals < ActiveRecord::Migration
+  def change
+    create_table :ezlocals do |t|
+      t.integer :business_id
+      t.datetime :force_update
+      t.text :secrets
+      t.integer, :ezlocal_category_id
+      t.timestamps
+    end
+  end
+end
