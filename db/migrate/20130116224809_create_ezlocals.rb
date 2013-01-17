e15788c83e4304a4a67bc12b7d23eae75583598d
Jonathan Jeffus <jjeffus@gmail.com>
2013-01-17 18:51:05 -0500

    fixing front page

--- db/migrate/20130116224809_create_ezlocals.rb
+++ db/migrate/20130116224809_create_ezlocals.rb
@@ -1,9 +1,9 @@
 class CreateEzlocals < ActiveRecord::Migration
   def change
     create_table :ezlocals do |t|
-      t.integer :business_id
+      t.integer  :business_id
       t.datetime :force_update
-      t.text :secrets
+      t.text     :secrets
       t.integer, :ezlocal_category_id
       t.timestamps
     end
