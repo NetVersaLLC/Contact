db16f97d59c364d0f7828107331cf9f482eb125c
Jonathan Jeffus <jjeffus@gmail.com>
2013-01-19 17:59:55 -0800

    adding new template!!

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def show_notice(message,subtext)
+    @message = message
+    @subtext = subtext
+    render :partial => 'pages/alert'
+  end
+  def show_notices(alert,notice)
+    if alert
+      show_notice(alert, '')
+    end
+    if notice
+      show_notice(notice, '')
+    end
+  end
 end
