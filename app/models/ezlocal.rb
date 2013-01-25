5c7c4bf8574e7bef50ed5eb6ae84e4dd523a1219
Jonathan Jeffus <jjeffus@gmail.com>
2013-01-24 19:11:30 -0800

    finally fixed the business model

--- app/models/ezlocal.rb
+++ app/models/ezlocal.rb
@@ -1,6 +1,4 @@
 class Ezlocal < ClientData
-attr_accessible :email, :ezlocal_category_id, :ezlocal_category
-
-	belongs_to :ezlocal_category
-
+  attr_accessible :email, :ezlocal_category_id
+  belongs_to :ezlocal_category
 end
