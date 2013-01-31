942e6691ee138ac8d4e0cc374051350ac1f86114
JoshWillis <blue74dot@gmail.com>
2013-01-31 00:42:00 +0000

    Adding some fixes for some payloads

--- sites/Yelp/SignUp/data_generator.rb
+++ sites/Yelp/SignUp/data_generator.rb
@@ -1,5 +1,6 @@
 data = {}
-data[ 'name' ]			= "Test9827398237479"#business.business_name
+catty = Yelp.where(:business_id => business.id).first
+data[ 'name' ]			= business.business_name
 data[ 'city' ]			= business.city
 data[ 'state' ]			= business.state
 data[ 'address' ]		= business.address
@@ -8,8 +9,7 @@ data[ 'zip' ]			= business.zip
 data[ 'phone' ]			= business.local_phone
 data[ 'website' ]		= business.company_website
 data[ 'email' ]			= business.bings.first.email
-google1 = GoogleCategory.where(:name => business.category1).first
-google2 = GoogleCategory.where(:name => business.category2).first
-data[ 'cat1' ] = YelpCategory.find(google1.yelp_category_id).name
-data[ 'cat2' ] = YelpCategory.find(google2.yelp_category_id).name
+
+data[ 'cat1' ] = catty.yelp_category.parent.name.gsub("\n", "")
+data[ 'cat2' ] = catty.yelp_category.name.gsub("\n", "")
 data
--- sites/Yippie/AddListing/data_generator.rb
+++ sites/Yippie/AddListing/data_generator.rb
@@ -1,7 +1,6 @@
 data = {}
-data[ 'category1' ]		= "Restaurant: Restaurants"#business.category1
-data[ 'category2' ]		= business.category2
-data[ 'category3' ]		= business.category3
+catty                 = Yippie.where(:business_id => business.id).first
+data[ 'category1' ] = catty.yippie_category.name.gsub("\r\r", "").gsub("\n", "")
 data[ 'state_name' ]		= business.state_name
 data[ 'state' ]			= business.state
 data[ 'city' ]			= business.city
