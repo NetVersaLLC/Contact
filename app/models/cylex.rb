dcb6247a8772dfb92b09c6e5a850961d3cee73a2
amitr1983 <amitr1983@github.com>
2013-02-05 17:44:11 +0000

    Adding citation for cylex

---
+++ app/models/cylex.rb
@@ -0,0 +1,6 @@
+class Cylex < ClientData  
+  attr_accessible :business_id, :created_at, :email, :force_update, :secretsi
+  virtual_attr_accessor :password
+  validates :password,
+        :presence => true
+end
