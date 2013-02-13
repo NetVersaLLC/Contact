42ea314376db746c11d92958ee72c939465f3694
JoshWillis <blue74dot@gmail.com>
2013-02-13 01:13:29 +0000

    Fixed Localndex

--- sites/Localndex/SearchBusiness/client_script.rb
+++ sites/Localndex/SearchBusiness/client_script.rb
@@ -5,16 +5,16 @@
 if @browser.text.include? "We couldn't find your business with the information provided."
 
 	if @chained
-		self.start("Listwns/PostAd")
+		self.start("Localndex/AddBusiness")
 	end	
-
+true
 elsif @browser.text.include? "Welcome"
 	@browser.button( :src => 'images/localndex_promo_advtoday.png').click
 
 	@browser.text_field( :id => 'ctl00_ContentPlaceHolder1_txtEmail').set data['email']
 	@browser.text_field( :id => 'ctl00_ContentPlaceHolder1_txtEmail2').set data['email']
 	@browser.text_field( :id => 'ctl00_ContentPlaceHolder1_txtPassword').set data['password']
-enter_captcha( data )
+enter_captcha2( data )
 	RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[username]' => data['email'], 'account[password]' => data['password'], 'model' => 'Localndex'
 
 end
--- sites/Localndex/SearchBusiness/data_generator.rb
+++ sites/Localndex/SearchBusiness/data_generator.rb
@@ -1,5 +1,5 @@
 data = {}
-data['phone']			= "9133381288"#business.local_phone.gsub("-", "")
+data['phone']			= business.local_phone.gsub("-", "")
 data['email']			= business.bings.first.email
 data['password']		= Yahoo.make_password
 data
