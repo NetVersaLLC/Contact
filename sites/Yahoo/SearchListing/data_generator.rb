506033d252dfcb2120b73c0fcbb5a5b15f186aa4
JoshWillis <blue74dot@gmail.com>
2013-02-18 22:23:04 +0000

    Adding Yahoo/SearchListing

---
+++ sites/Yahoo/SearchListing/data_generator.rb
@@ -0,0 +1,8 @@
+data = {}
+
+data[ 'business' ]              = "Paddy O'Quigley's"#business.business_name
+data[ 'city' ]              = business.city
+data[ 'state_short' ]       = business.state
+data[ 'citystate' ] = data[ 'city' ] + ", " + data[ 'state_short' ]
+
+data
