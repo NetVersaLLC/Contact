0a202b1b4e008329878f7e4bc057e62eb4ce0a35
JoshWillis <blue74dot@gmail.com>
2013-02-18 23:28:28 +0000

    Adding Manta/SearchListing

--- sites/Manta/SearchListing/data_generator.rb
+++ sites/Manta/SearchListing/data_generator.rb
@@ -1,8 +1,7 @@
 data = {}
-
-data[ 'business' ]              = business.business_name
+data[ 'business' ]          = business.business_name
 data[ 'city' ]              = business.city
 data[ 'state_short' ]       = business.state
 data[ 'citystate' ] = data[ 'city' ] + ", " + data[ 'state_short' ]
-
+data[ 'query' ] = data['business'] + ", " + data[ 'citystate' ]
 data
