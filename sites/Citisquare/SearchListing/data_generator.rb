45f4a601f6f40eec0c62b23bf3be567f2d4bc036
JoshWillis <blue74dot@gmail.com>
2013-02-19 19:46:34 +0000

    SearchListing for Zippro,Shopcity,Citisquare

--- sites/Citisquare/SearchListing/data_generator.rb
+++ sites/Citisquare/SearchListing/data_generator.rb
@@ -2,6 +2,7 @@ data = {}
 data[ 'business' ]          = business.business_name
 data[ 'city' ]              = business.city
 data[ 'state_short' ]       = business.state
+data[ 'zip' ]       = business.zip
 data[ 'citystate' ] = data[ 'city' ] + ", " + data[ 'state_short' ]
 data[ 'phone' ]             = business.local_phone
 data
---
+++ sites/Shopcity/SearchListing/data_generator.rb
@@ -0,0 +1,10 @@
+data = {}
+data[ 'country' ]		= "United States"
+data[ 'state' ]			= business.state_name
+data[ 'city' ]			= business.city
+data[ 'cityState' ]		= data['city'] + ", " + data['state']
+data[ 'state_name' ]		= business.state_name
+data[ 'city' ]			= business.city
+data[ 'business' ]		= business.business_name
+data
+
