d74fc97dbd2a443e6f35bb842c88ead598238a6c
Jonathan Jeffus <jjeffus@gmail.com>
2013-02-19 17:12:03 -0800

    removing category class

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -14,29 +14,27 @@ ActiveAdmin.register_page "Categories" do
       "Category: #{business.category1}"
     end
     form(:action => '/bunnies', :method => 'post') do
-      cache('citation_list_form') do
-        Business.citation_list.each do |data|
-          data[2].each do |row|
-            if row[0] == 'select'
-              klass = row[1].classify.constantize
-              next if klass == YahooCategory
-              category_name = ''
-              if business.send(data[1]).count > 0
-                site = business.send(data[1]).first
-                if site
-                  category = site.send("#{row[1]}_id")
-                  if category
-                    category_name = klass.find(category).make_category
-                  end
+      Business.citation_list.each do |data|
+        data[2].each do |row|
+          if row[0] == 'select'
+            klass = row[1].classify.constantize
+            next if klass == YahooCategory
+            category_name = ''
+            if business.send(data[1]).count > 0
+              site = business.send(data[1]).first
+              if site
+                category = site.send("#{row[1]}_id")
+                if category
+                  category_name = klass.find(category).make_category
                 end
               end
-              render :partial => 'show', :locals => { :klass => klass, :category_name => category_name }
             end
+            render :partial => 'show', :locals => { :klass => klass, :category_name => category_name }
           end
         end
-        input(:type => :hidden, :id => 'business_id', :name => 'business_id', :value => business.id)
-        input(:type => :button, :onclick => 'window.submitCategory()', :value => 'Submit')
       end
+      input(:type => :hidden, :id => 'business_id', :name => 'business_id', :value => business.id)
+      input(:type => :button, :onclick => 'window.submitCategory()', :value => 'Submit')
     end
     res = ActiveRecord::Base.connection.execute("SELECT id FROM businesses WHERE id != #{business.id} AND categorized IS NULL")
     if res.count > 0
