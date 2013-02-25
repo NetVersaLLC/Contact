4cce20c913dd8c9e1afc82d1ded44a7aa1d31af7
amitr1983 <amitr1983@github.com>
2013-02-25 18:00:17 +0000

    Adding payload for discoverowntown

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130218032710) do
+ActiveRecord::Schema.define(:version => 20130225175145) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
