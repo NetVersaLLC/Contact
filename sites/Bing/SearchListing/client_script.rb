2dde818d16b05ed52bb3fe4bfd939bc77c06ddb0
JoshWillis <blue74dot@gmail.com>
2013-02-25 17:04:01 +0000

    Some fixes for SearchListing working on Headless

--- sites/Bing/SearchListing/client_script.rb
+++ sites/Bing/SearchListing/client_script.rb
@@ -1,4 +1,4 @@
-def listing_already_exists
+def listing_already_exists2
 
   #@claim_business_link = @browser.div( :text , 'Claim' )
   #@not_found_text = @browser.div( :class, 'LiveUI_Area_NoMatches' )
@@ -6,7 +6,7 @@ def listing_already_exists
   def not_found_text; @browser.text.include? 'NO MATCHES FOUND' end
 
   # if claim check is first and no results found it waits for 30 seconds and fails
-  watir_must do not_found_text or claim_business_link.exists? end
+  Watir::Wait.until {not_found_text or claim_business_link.exists?}
 
   if not_found_text
     puts 'No results found'
@@ -20,10 +20,31 @@ def listing_already_exists
   
 end
 
+
+def search_for_business2( business )
+
+  @browser.goto( 'http://www.bing.com/businessportal/' )
+  puts 'Search for the ' + business[ 'name' ] + ' business at ' + business[ 'city' ] + ' city'
+  @browser.link( :text , 'Get Started Now!' ).click
+
+  #sleep 4 # seems that div's are not loaded quickly simetimes
+  @browser.div( :class , 'LiveUI_Area_Find___Business' ).text_field( :class, 'LiveUI_Field_Input' ).when_present.click
+  @browser.div( :class , 'LiveUI_Area_Find___Business' ).text_field( :class, 'LiveUI_Field_Input' ).flash
+  @browser.div( :class , 'LiveUI_Area_Find___Business' ).text_field( :class, 'LiveUI_Field_Input' ).focus
+  @browser.div( :class , 'LiveUI_Area_Find___Business' ).text_field( :class, 'LiveUI_Field_Input' ).set business[ 'name' ]
+  @browser.div( :class , 'LiveUI_Area_Find___City' ).text_field( :class, 'LiveUI_Field_Input' ).click
+  @browser.div( :class , 'LiveUI_Area_Find___City' ).text_field( :class, 'LiveUI_Field_Input' ).set business[ 'city' ]
+  @browser.div( :class , 'LiveUI_Area_Find___State' ).text_field( :class, 'LiveUI_Field_Input' ).click
+  @browser.div( :class , 'LiveUI_Area_Find___State' ).text_field( :class, 'LiveUI_Field_Input' ).set business[ 'state_short' ]
+  @browser.div( :class , 'LiveUI_Area_Search_Button LiveUI_Short_Button_Medium' ).click
+  #sleep 4
+  
+end
+
 businessFound = []
 
-search_for_business( data )
-result = listing_already_exists()
+search_for_business2( data )
+result = listing_already_exists2()
 
 if result == true  
   businessFound = [:listed,:unclaimed]  
@@ -31,4 +52,5 @@ else
   businessFound = [:unlisted]
 end
 
-businessFound
+
+STDERR.puts businessFound.inspect
