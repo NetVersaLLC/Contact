e9aec3e6a45b83dd56a54a0e1de46832f70ddfc2
JoshWillis <blue74dot@gmail.com>
2013-02-28 15:41:56 +0000

    Optimized SearchListings

--- sites/Hyplo/SearchListing/client_script.rb
+++ sites/Hyplo/SearchListing/client_script.rb
@@ -6,6 +6,8 @@
 @browser.button( :text => 'Search').click
 sleep(5)
 
+Watir::Wait.until { @browser.text.include? "Hyperlocal sites for" }
+
 if @browser.div(:class => 'span12 foundsite').exists?
     if @browser.link( :text => /#{data['business']}/).exists?
         businessFound = [:listed,:claimed]
--- sites/Showmelocal/SearchListing/client_script.rb
+++ sites/Showmelocal/SearchListing/client_script.rb
@@ -1,20 +1,14 @@
-@browser.goto('http://www.showmelocal.com/')
+@browser.goto("http://www.showmelocal.com/local_search.aspx?q=#{data['businessfixed']}&s=#{data['state_short']}&c=#{data['city']}")
 
-@browser.text_field( :id => '_ctl5__ctl0_txtWhat').set data['business']
-@browser.text_field( :name => '_ctl5:_ctl0:txtWhere').set data['citystate']
 
-@browser.button( :name => '_ctl5:_ctl0:cmdSearch').click
-sleep(5)
-
-if @browser.link( :text => data['business']).exists?
-    @browser.link( :text => /#{data['business']}/).click
-        sleep(5)
+if @browser.div(:class => 'serachresult').div(:class => 'h').link( :text => /#{data['business']}/i).exists?
+    @browser.div(:class => 'serachresult').div(:class => 'h').link( :text => /#{data['business']}/i).click
+        Watir::Wait.until { @browser.link(:id => '_ctl5_hlBusinessName').exists? }
       if @browser.link( :id => '_ctl5_hlAreYourTheOwner').exists?          
           businessFound = [:listed,:unclaimed]
       else
           businessFound = [:listed,:claimed]
       end
- 
  else
     businessFound = [:unlisted]
  end  
