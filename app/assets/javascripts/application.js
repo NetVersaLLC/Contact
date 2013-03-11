f2d54aa7f437e111058ab84d0d309c048539cda1
Jonathan Jeffus <jjeffus@gmail.com>
2013-03-11 16:10:05 -0700

    new signup and checkout page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,11 +1,11 @@
 
 //= require dummy
 //= require jquery_ujs
-//= require jquery-ui
-//= require jquery_nested_form
 //= require rails.validations
-//= require rv
 //= require bootstrap.min
 //= require businesses
 //= require jquery.fineuploader-3.3.0.min
+
+//= require checkout
+//= require jquery.payment
 
