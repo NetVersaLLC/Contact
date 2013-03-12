06c25ecf71a901347976479998283aa617abf899
Jonathan Jeffus <jjeffus@gmail.com>
2013-03-12 10:59:22 -0700

    new form and subscription system

--- app/models/coupon.rb
+++ app/models/coupon.rb
@@ -2,12 +2,4 @@ class Coupon < ActiveRecord::Base
   attr_accessible :code, :name, :login, :password, :percentage_off, :label_id
   belongs_to :label
 
-  def get_gateway
-    gateway = ActiveMerchant::Billing::Base.gateway(:authorize_net).new(
-      :login    => self.login,
-      :password => self.password,
-      :test     => true
-    )
-    gateway
-  end
 end
