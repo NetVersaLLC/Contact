2c0fc92cfb07df492593c086bcb7f3c025710e30
JoshWillis <blue74dot@gmail.com>
2013-03-12 00:53:18 +0000

    Adding Ezlocal/UpdateListing, ExpertFocus/UpdateListing and fixes for Aol and Crunchbase

--- sites/Crunchbase/SignUp/data_generator.rb
+++ sites/Crunchbase/SignUp/data_generator.rb
@@ -3,7 +3,9 @@ data['name'] 		= business.contact_first_name + ' ' + business.contact_last_name
 data['username'] 	= business.bings.first.email.split("@")[0]
 data['email'] 		= business.bings.first.email
 data['password'] 	= Yahoo.make_password
-data['twitter'] 	= 'happygolucky'#business.twitters.first.username
+
+data['twitter'] 	= ""#business.twitters.first.username
+
 data['homepage'] 	= business.company_website
 data['use_facebook'] 	= false
 data
