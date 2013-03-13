d9723a134a78da1d4ed5ca0f238fc1e310a4d6db
JoshWillis <blue74dot@gmail.com>
2013-03-13 16:16:44 +0000

    Digabusiness UpdateListing and updates/fixes

--- sites/Digabusiness/AddListing/data_generator.rb
+++ sites/Digabusiness/AddListing/data_generator.rb
@@ -3,13 +3,15 @@ catty                       = Digabusiness.where(:business_id => business.id).fi
 data[ 'category1' ]          = catty.digabusiness_category.parent.name.gsub("\n", "")
 data[ 'category2' ]       = catty.digabusiness_category.name.gsub("\n", "")
 
+data['password']		= business.digabusinesses.first.password
+data[ 'email' ]			= business.digabusinesses.first.email
+
 data[ 'business' ]		= business.business_name
 data[ 'website' ]		= business.company_website
 data[ 'description' ]		= business.business_description
 data[ 'fname' ]			= business.contact_first_name
 data[ 'lname' ]			= business.contact_last_name
 data[ 'fullname' ]		= data[ 'fname' ] + ' ' + data[ 'lname' ]
-data[ 'email' ]			= business.bings.first.email
 data[ 'addressComb' ]		= business.address + "  " + business.address2
 data[ 'city' ]			= business.city
 data[ 'state_name' ]		= business.state_name
--- sites/Digabusiness/shared.rb
+++ sites/Digabusiness/shared.rb
@@ -1,18 +1,24 @@
-def solve_captcha
-  image = "#{ENV['USERPROFILE']}\\citation\\matchpoint_captcha.png"
-  obj = @browser.img( :xpath, '/html/body/div[3]/div[2]/div/div[2]/div/form/table/tbody/tr[28]/td[2]/img' )
+def solve_captcha	
+  image = "#{ENV['USERPROFILE']}\\citation\\digabusiness_captcha.png"
+  puts("image saved?")
+  obj = @browser.img( :title, 'Visual Confirmation Security Code' )
+  puts("image saved.")
   puts "CAPTCHA source: #{obj.src}"
   puts "CAPTCHA width: #{obj.width}"
   obj.save image
-
+	puts("About to solve the code")
   CAPTCHA.solve image, :manual
 end
 
 def enter_captcha( data )
+	puts("Is this called?")
 	capSolved = false
 	count = 1
 	until capSolved or count > 5 do
-		captcha_code = solve_captcha	
+
+		captcha_code = solve_captcha
+
+		puts(captcha_code)
 		@browser.text_field( :id => 'CAPTCHA').set captcha_code
 		@browser.button( :name => 'submit').click
 		sleep(2)
@@ -28,4 +34,42 @@ def enter_captcha( data )
 	end
 end
 
+
+def solve_captcha_signup
+  image = "#{ENV['USERPROFILE']}\\citation\\digabusiness_captcha.png"
+  obj = @browser.img( :xpath, '//*[@id="padding"]/form/table/tbody/tr[8]/td[2]/img' )
+  puts "CAPTCHA source: #{obj.src}"
+  puts "CAPTCHA width: #{obj.width}"
+
+  obj.save image
+  CAPTCHA.solve image, :manual
+end
+
+def enter_captcha_signup( data )
+	capSolved = false
+	count = 1
+	until capSolved or count > 5 do
+		captcha_code = solve_captcha_signup	
+		@browser.text_field( :id => 'CAPTCHA').set captcha_code
+		@browser.button( :name => 'submit').click
+		sleep(2)
+		if not @browser.text.include? "Invalid code."
+			capSolved = true
+		end	  
+	count+=1
+	end
+	if capSolved == true
+		true
+	else
+		throw("Captcha was not solved")
+	end
+end
+
+
+def sign_in(data)
+	@browser.goto("http://www.digabusiness.com/login.php")
+	@browser.text_field(:name => 'user').set data['email']
+	@browser.text_field(:name => 'pass').set data['password']
+	@browser.button(:name => 'submit').click
+end
 
