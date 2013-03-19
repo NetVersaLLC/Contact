35875a6ac66ef90ce1a6aa5a24fa0f1fc8ab9a52
Jonathan Jeffus <jjeffus@gmail.com>
2013-03-18 17:18:24 -0700

    changing to spinach

--- db/migrate/20130318182455_move_transaction_id_to_transaction_event_id.rb
+++ db/migrate/20130318182455_move_transaction_id_to_transaction_event_id.rb
@@ -1,6 +1,6 @@
 class MoveTransactionIdToTransactionEventId < ActiveRecord::Migration
   def change
-    rename_column :subscriptions, :transaction_id, :transaction_event_id
-    rename_column :payments,      :transaction_id, :transaction_event_id
+    rename_column :subscriptions, :transaction_id,       :transaction_event_id
+    add_column    :payments,      :transaction_event_id, :integer
   end
 end
