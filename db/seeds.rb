0d9febe10e11f1b95cadcf5d7f26ddd1f02bc317
nahrae <jinnahrae@gmail.com>
2013-03-22 18:54:25 +0800

    Fix problems on runnig db-migrations when deploying project from ground

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,12 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Added by Refinery CMS Pages extension
-Refinery::Pages::Engine.load_seed
+# Refinery::Pages::Engine.load_seed
+
+
+Label.new(:name=>'TownCenter', :domain=>'towncenter.com').save(:validate=>false)
+
+Package.new(:name=>'Starter', :price=>99.99, 
+  :short_description => "This is the short description, it's meant to be one or two sentences.",
+  :description => "This is the long description of this package. It can contain HTML and could be several pages if that made sense.")
+  .save(:validate=>false)
