96fd9d407338659a155817bb0035f3b948922c71
Jonathan Jeffus <jjeffus@gmail.com>
2013-03-26 08:19:52 -0700

    adding search tracking

--- app/controllers/scan_controller.rb
+++ app/controllers/scan_controller.rb
@@ -9,7 +9,7 @@ class ScanController < ApplicationController
       next unless File.exists? Rails.root.join('sites', site[0], 'SearchListing')
       resp.push site[0]
     end
-    resp = %w/Yahoo Bing Yelp/ #%w/Bing Yelp Yahoo Ezlocal Justclicklocal Yellowassistance Ebusinesspages Citisquare ShopCity Zippro Yellowee Digabusiness Localizedbiz Showmelocal Expressbusinessdirectory/
+    resp = %w/Yahoo Citisquare Cornerstonesworld Kudzu/ #%w/Bing Yelp Yahoo Ezlocal Justclicklocal Yellowassistance Ebusinesspages Citisquare ShopCity Zippro Yellowee Digabusiness Localizedbiz Showmelocal Expressbusinessdirectory/
     render json: resp
   end
   def site
@@ -17,6 +17,13 @@ class ScanController < ApplicationController
     res   = @scan.run()
     model = params[:id].constantize
     bid   = params[:business_id]
+    Search.create do |s|
+      s.name    = params[:name]
+      s.zip     = params[:zip]
+      s.city    = params[:city]
+      s.address = params[:address]
+      s.phone   = params[:phone]
+    end
     if bid
       business = Business.find(bid)
       count    = CompletedJob.find_by_sql("select count(*) from completed_jobs where business_id=#{business.id} and name rlike '^#{model}/';")
---
+++ app/models/search.rb
@@ -0,0 +1,3 @@
+class Search < ActiveRecord::Base
+  attr_accessible :address, :city, :name, :phone, :zip
+end
