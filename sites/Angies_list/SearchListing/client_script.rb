a8b0e4c517fb23f3c92645b8255f364e58af5d36
JoshWillis <blue74dot@gmail.com>
2013-03-27 21:04:36 +0000

    Fixed Bing and Angies_list searchlistings

--- sites/Angies_list/SearchListing/client_script.rb
+++ sites/Angies_list/SearchListing/client_script.rb
@@ -1,8 +1,7 @@
 def search_business(data)
-	@browser.wait_until {@browser.div(:class , 'RegistrationLightbox').exist? }
+	Watir::Wait.until {@browser.div(:class , 'RegistrationLightbox').exist? }
 	result_count = @browser.table(:class, 'wide100percent').rows.length
 
-        #Search for matching result
 	for n in 1...result_count
 		result = @browser.table(:class, 'wide100percent')[n].text
 		if result.include?(data[ 'business' ])
@@ -17,16 +16,13 @@ end
  @browser.goto( 'https://business.angieslist.com/Registration/Registration.aspx' )
   @browser.text_field(:id => /CompanyName/).set data[ 'business' ]
   @browser.text_field(:id => /CompanyZip/).set data[ 'zip' ]
-  @browser.image(:alt,'Search').click
-  
-  Watir::Wait.until { @browser.text.include? "Select a Company" }
-  #Check if business already listed
-  @error_msg = @browser.span(:class,'errortext')
-  @no_match_text = 'No companies were found. Try entering partial information in the search fields.'
+  @browser.image(:alt,'Search').click  
+@browser.screenshot.save 'screenshot1.png'
+sleep(2)
+  Watir::Wait.until { @browser.div(:id => 'ctl00_ContentPlaceHolderMainContent_SimpleRegistrationWizard_SelectCompanyControl_SelectACompanyContainer') }
+  @error_msg = @browser.span(:class,'errortext')    
 
-if @browser.text.include?(@no_match_text)
-  businessFound = [:unlisted]
-elsif search_business(data)
+if search_business(data)
     businessFound = [:listed,:unclaimed]
 else
 businessFound = [:unlisted]
