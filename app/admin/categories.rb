646482d6924b22692776be0a21753c7a8f5deb53
Jonathan Jeffus <jjeffus@gmail.com>
2013-03-27 22:19:21 -0700

    fixing category system

--- app/admin/categories.rb
+++ app/admin/categories.rb
@@ -19,16 +19,30 @@ ActiveAdmin.register_page "Categories" do
           if row[0] == 'select'
             klass = row[1].classify.constantize
             next if klass == YahooCategory
-            if business.send(data[1]).count > 0
-              site = business.send(data[1]).first
-              if site
-                category = site.send("#{row[1]}_id")
-                if category
-                  category_name = klass.find(category).make_category
-                end
+            script(:src => "/categories/#{klass}.js") do
+            end
+            category_name = nil
+            category_id   = nil
+            res = ActiveRecord::Base.connection.execute "SELECT #{row[1]}_id FROM #{data[1]} WHERE business_id=#{business.id} AND #{row[1]}_id IS NOT NULL"
+            res.each do |row|
+              logger.info "Row: #{row.inspect}"
+              category_name = klass.find(row[0]).name
+              category_id   = row[0]
+            end
+            unless category_name
+              category_name = '<button onclick="loadCategory(\''+klass.to_s+'\')">Select</button>'
+            end
+            div(:class => 'category_show') do
+              h2 do
+                klass.to_s
+              end
+              h3(:id => "category_#{klass.to_s}") do 
+                raw category_name
+              end
+              input(:type => 'hidden', :name => "category[#{klass.to_s}]", :id => "category_#{klass.to_s}", :value => category_id)
+              div(:class => 'selector', :id => "selector_#{klass.to_s}") do
               end
             end
-            render :partial => 'show', :locals => { :klass => klass, :category_name => category_name }
           end
         end
       end
