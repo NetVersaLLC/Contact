05b806a0da720a88f5361c25c90b35fbadc366c7
Weston Platter <westonplatter@gmail.com>
2013-03-31 02:06:13 -0500

    gh issue #3 - hhtp PUT user input after user moves to next input

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,12 +2,10 @@
 //= require dummy
 //= require jquery_ujs
 //= require jquery-ui
-//= require rv
 //= require rails.validations
 //= require bootstrap.min
 //= require businesses
 //= require jquery.fineuploader-3.3.0.min
-
 //= require jquery.payment
 //= require checkout
 
