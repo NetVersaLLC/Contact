a3b3634bae7270c903280422a07c4135f583bc02
JoshWillis <blue74dot@gmail.com>
2013-04-03 18:18:19 +0000

    Some fixes for Matchpoint

--- sites/Matchpoint/shared.rb
+++ sites/Matchpoint/shared.rb
@@ -32,3 +32,42 @@ def enter_captcha( data )
 	end
 end
 
+
+
+def solve_captcha2
+  image = "#{ENV['USERPROFILE']}\\citation\\matchpoint_captcha.png"
+  obj = @browser.img( :xpath, '//*[@id="register"]/div[1]/div[5]/div[2]/img' )
+  puts "CAPTCHA source: #{obj.src}"
+  puts "CAPTCHA width: #{obj.width}"
+  obj.save image
+
+  CAPTCHA.solve image, :manual
+end
+
+def enter_captcha2( data )
+
+	capSolved = false
+	count = 1
+	until capSolved or count > 5 do
+		captcha_code = solve_captcha2
+		@browser.text_field( :name => 'password').focus
+		@browser.text_field( :name => 'password').set data['password']
+		@browser.text_field( :name => 'passwordAgain').focus
+		@browser.text_field( :name => 'passwordAgain').set data['password']
+		@browser.text_field( :name, 'verifyWord').focus
+		@browser.text_field( :name, 'verifyWord').set captcha_code
+		
+		@browser.button( :value => 'Create Account').click
+		sleep(2)
+		if not @browser.text.include? "Word Verification do not match"
+			capSolved = true
+		end	  
+	count+=1
+	end
+	if capSolved == true
+		true
+	else
+		throw("Captcha was not solved")
+	end
+end
+
