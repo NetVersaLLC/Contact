fa75ad27d45c6ebd197a63288187b757f53d502d
JoshWillis <blue74dot@gmail.com>
2013-04-08 19:31:15 +0000

    Redone and added UpdateListing for Showmelocal

--- sites/Showmelocal/SignUp/client_script.rb
+++ sites/Showmelocal/SignUp/client_script.rb
@@ -1,29 +1,31 @@
 @browser.goto( 'http://www.showmelocal.com/businessregistration.aspx' )
 puts(data['category'])
 #business info
-@browser.text_field( :id => 'txtBusinessName').set data[ 'business' ]
-@browser.select_list( :id => '_ctl7_cboCategory').select data[ 'category' ]
-@browser.text_field( :id => 'txtType').set data[ 'type' ]
-@browser.text_field( :id => '_ctl4_txtPhone1').set data[ 'phone' ]
-@browser.text_field( :id => '_ctl3_txtAddress1').set data[ 'address' ]
-@browser.text_field( :id => '_ctl3_txtAddress2').set data[ 'address2' ]
-@browser.text_field( :id => '_ctl3_txtCity').set data[ 'city' ]
-@browser.select_list( :id => '_ctl3_cboState').select data[ 'state' ]
-@browser.text_field( :id => '_ctl3_txtZip').set data[ 'zip' ]
+
 
 #personal info
-@browser.text_field( :id => '_ctl2_txtFirstName').set data[ 'fname' ]
-@browser.text_field( :id => '_ctl2_txtLastName').set data[ 'lname' ]
-@browser.text_field( :id => '_ctl2_txtEmail').set data[ 'email' ]
-@browser.text_field( :id => '_ctl2_txtEmailConfirm').set data[ 'email' ]
-@browser.text_field( :id => '_ctl2_txtPassword').set data[ 'password' ]
-@browser.text_field( :id => '_ctl2_txtPasswordsConfirmed').set data[ 'password' ]
+@browser.text_field( :id => '_ctl0_txtFirstName').set data[ 'fname' ]
+@browser.text_field( :id => '_ctl0_txtLastName').set data[ 'lname' ]
+@browser.text_field( :id => '_ctl0_txtEmail').set data[ 'email' ]
+#@browser.text_field( :id => '_ctl0_txtEmailConfirm').set data[ 'email' ]
+@browser.text_field( :id => '_ctl0_txtPassword').set data[ 'password' ]
 
-@browser.checkbox( :id => 'chkAgree').click
+	enter_captcha( data )
 
-puts(data['password'])
+@browser.text_field( :id => '_ctl0_txtBusinessName').when_present.set data[ 'business' ]
+@browser.text_field( :id => '_ctl0_txtBusinessType').set data[ 'category' ]
+#@browser.text_field( :id => 'txtType').set data[ 'type' ]
+@browser.text_field( :id => '_ctl0_txtPhone').set data[ 'phone' ]
+@browser.text_field( :id => '_ctl0_txtAddress').set data[ 'address' ]
+@browser.text_field( :id => '_ctl0_txtAddress2').set data[ 'address2' ]
+#@browser.text_field( :id => '_ctl3_txtCity').set data[ 'city' ]
+#@browser.select_list( :id => '_ctl3_cboState').select data[ 'state' ]
+@browser.text_field( :id => '_ctl0_txtZip').set data[ 'zip' ]
 
-enter_captcha( data )
+
+#@browser.checkbox( :id => 'chkAgree').click
+	enter_captcha( data )
+
 
 RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[username]' => data['email'], 'account[password]' => data['password'], 'model' => 'Showmelocal'
 	if @chained
