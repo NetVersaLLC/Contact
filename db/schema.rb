d7d5e96eadef61062672afa5b1bf7203231938fb
JoshWillis <blue74dot@gmail.com>
2013-04-09 17:14:06 +0000

    Staylocal udpates

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130408211548) do
+ActiveRecord::Schema.define(:version => 20130408235128) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
@@ -628,6 +628,7 @@ ActiveRecord::Schema.define(:version => 20130408211548) do
     t.integer  "merchantcircle_category_id"
     t.integer  "mojopages_category_id"
     t.integer  "spotbusiness_category_id"
+    t.integer  "staylocal_category_id"
   end
 
   add_index "google_categories", ["name"], :name => "index_google_categories_on_name"
@@ -1339,13 +1340,24 @@ ActiveRecord::Schema.define(:version => 20130408211548) do
 
   add_index "spotbusinesses", ["business_id"], :name => "index_spotbusinesses_on_business_id"
 
+  create_table "staylocal_categories", :force => true do |t|
+    t.integer  "parent_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "staylocal_categories", ["name"], :name => "index_staylocal_categories_on_name"
+  add_index "staylocal_categories", ["parent_id"], :name => "index_staylocal_categories_on_parent_id"
+
   create_table "staylocals", :force => true do |t|
     t.datetime "force_update"
     t.text     "secrets"
     t.integer  "business_id"
     t.string   "email"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",            :null => false
+    t.datetime "updated_at",            :null => false
+    t.integer  "staylocal_category_id"
   end
 
   add_index "staylocals", ["business_id"], :name => "index_staylocals_on_business_id"
--- sites/Staylocal/AddListing/client_script.rb
+++ sites/Staylocal/AddListing/client_script.rb
@@ -1,11 +1,7 @@
-@url = 'https://www.staylocal.org/node/add/business-listing'
-@browser.goto(@url)
+sign_in(data)
+sleep(2)
+@browser.goto("https://www.staylocal.org/node/add/business-listing")
 
-
-  @browser.text_field(:name => 'name').set data[ 'username' ]
-  @browser.text_field(:name => 'mail').set data[ 'email' ]
-  @browser.text_field(:name => 'pass[pass1]').set data[ 'password' ]
-  @browser.text_field(:name => 'pass[pass2]').set data[ 'password' ]
   @browser.text_field(:name => 'title').set data[ 'business' ]
   @browser.text_field(:name => /street/).set data[ 'address' ]
   @browser.text_field(:name => /city/).set data[ 'city' ]
@@ -24,25 +20,15 @@
   
   #Enter Captcha Code
   # @browser.text_field(:name => 'recaptcha_response_field').set captcha_textcha
-  #@browser.button(:value => 'Save').click
+@browser.button(:value => 'Save').click
   
-enter_captcha
-
-
-Watir::Wait.until{ @browser.div(:class => 'messages error').exists? or @browser.text.include? "A validation e-mail has been sent to your e-mail address"}
- 
+Watir::Wait.until{ @browser.div(:class => 'messages error').exists? or @browser.text.include? "Initial draft created, pending publication"}
+ sleep(100)
   
   if @browser.text.include? "A validation e-mail has been sent to your e-mail address"
-    puts "Initial Business registration is successful"
-    RestClient.post "#{@host}/accounts.json?auth_token=#{@key}&business_id=#{@bid}", 'account[email]' => data[ 'username' ], 'model' => 'Staylocal'
-    
-    if @chained
-      self.start("Staylocal/Verify")
-    end
-
     true
   else
-    throw "Initial Business registration is Unsuccessful"
+    throw "Adding business failed"
   end
 
 
