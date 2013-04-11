9bd759238fb7375c829aafa93b5b5791cf61709d
Ted Palmisciano <tpalmisciano@gmail.com>
2013-04-11 15:01:16 -0400

    adding tests

--- features/step_definitions/business_form_steps.rb
+++ features/step_definitions/business_form_steps.rb
@@ -1,4 +1,4 @@
-Given /I have signed in as [a]n? (admin|reseller|user)/ do |role|
+Given /I have signed in as [a]n? (admin|reseller|employee|owner)/ do |role|
   label = Label.new(name: 'towncenter', login: 'login', password: 'pswd', domain: 'domain' )
   label.logo_file_name = "nothing" 
   label.save!
@@ -22,3 +22,18 @@ Given /I am on the edit business page/ do
   page.has_text? 'Business Details' 
 end 
 
+Given /I enter a zip code to Search by Zip/ do 
+  fill_in 'Search by Zip', with: '92626' 
+end 
+
+And /I select a different form element/ do 
+  page.first("div").click() 
+end 
+
+And /I go to the edit business page/ do 
+  page.click_link "Edit Business" 
+end 
+
+Then /I should see its city and state/ do 
+  pending("need to figure out a way to wait for the ajax to return.") 
+end 
