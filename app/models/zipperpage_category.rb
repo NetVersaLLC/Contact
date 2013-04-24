86819de1a108e309f0ada2f2c26968ac43b26bb7
Jonathan Jeffus <jjeffus@gmail.com>
2013-04-24 12:00:35 -0700

    fixing typo in zipperpage category

--- app/models/zipperpage_category.rb
+++ app/models/zipperpage_category.rb
@@ -2,5 +2,5 @@ class ZipperpageCategory < SiteCategory
   attr_accessible :name, :parent_id
   acts_as_tree :order => :name
   belongs_to :google_category
-  as_many :zipperpages 
+  has_many :zipperpages 
 end
