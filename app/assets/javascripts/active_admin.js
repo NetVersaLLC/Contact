cb36f7fdaefb03cc72e38ffd9e4dc943536056f7
kashif <kashif@kashif-HP-ProBook-4520s.(none)>
2013-05-01 07:06:19 -0400

    fixed reseller/show hide password

--- app/assets/javascripts/active_admin.js
+++ app/assets/javascripts/active_admin.js
@@ -3,7 +3,20 @@
 //= require jquery
 //= require jquery_ujs
 //= require aa-jquery-ui
+//= require jquery-migrate-1.1.1
+//= require jquery-ui-timepicker-addon
 //= require active_admin_clients
 //= require active_admin_packages
 //= require active_admin_categories
+
+
+
+
+
+
+
+
+
+
+
 
