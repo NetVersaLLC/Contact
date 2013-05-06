f4af8341a9b6f8387edfc07d74bf158ac5668668
David Collazo <davidcollazo@gmail.com>
2013-05-06 14:15:20 -0700

    added model updating to Vitelity number rerouter

--- account_creator/reroute_number.rb
+++ account_creator/reroute_number.rb
@@ -8,7 +8,10 @@ v = Vitelity.new('netv_jon', 'hotfiya6!')
 
 vitelity_number = VitelityNumber.where (:business_id => ARGV.shift ).first
 
-response = v.callfw( {:did => number, :forward => ARGV.shift }).first 
+destination = ARGV.shift
+
+response = v.callfw( {:did => vitelity_number.did, :forward => destination  }).first 
 ap response
 
+vitelity_number.forwards_to = destination
 vitelity_number.save!
