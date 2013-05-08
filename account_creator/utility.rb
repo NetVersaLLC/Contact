a8e43cfcd0647c4ef4074e07c1792d064741ad54
Jonathan Jeffus <jjeffus@gmail.com>
2013-05-08 15:27:01 -0700

    moving get business out of gemfile require

--- account_creator/utility.rb
+++ account_creator/utility.rb
@@ -51,30 +51,7 @@ module Utility
 
   def self.get_address( number )
     # ywsid = 'IRRAQuDAiFqFCPGrPzyA3Q'
-    client = Yelp::Client.new
-    request = Yelp::V2::Search::Request::Location.new(
-     :term    => 'cable provider',
-     :city => number['ratecenter'],
-     :state => number['state'],
-     :location => "#{number['ratecenter']}, #{number['state']}",
-     :consumer_key => 'guxExEGc6q_CGmo8Xv44Bg',
-     :consumer_secret => 'oxYRwyMK7jL2zVrMUjINlVti914',
-     :token => '-neQfJo0y6Ie_mHaR_jq7UfiC2SHZPXN',
-     :token_secret => '-8ruinaW1nedbv7fxTJ3d7NAihE'
-    )
-    response = client.search(request)
-    # response = YAML::load_file('request.yml')
-    f = File.open("request.yml", 'w')
-    YAML::dump(response, f)
-    business_hash = {}
-    response['businesses'].each do |business|
-      business_hash['address'] = response["businesses"][0]['location']["address"].join(" ")
-      business_hash['city']    = response["businesses"][0]['location']["city"]
-      business_hash['state']   = response["businesses"][0]['location']["state_code"]
-      business_hash['zip']     = response["businesses"][0]['location']["postal_code"]
-      break
-    end
-    business_hash
+    JSON.parse( `./account_creator/get_business.rb "#{number['ratecenter']}" "#{number['state']}"`)
   end
 
   def self.company_faker
