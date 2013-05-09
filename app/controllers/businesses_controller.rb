8d3b5e14925055d16200a9aaa38989591e12a8da
Jonathan Jeffus <jjeffus@gmail.com>
2013-05-09 08:15:10 -0700

    fixing business form

--- app/controllers/businesses_controller.rb
+++ app/controllers/businesses_controller.rb
@@ -105,7 +105,10 @@ class BusinessesController < ApplicationController
       BusinessFormEdit.where(:user_id => current_user.id).delete_all
       redirect_to business_url(@business), :notice => 'Congratulations! Your business profile has been created!' 
     else 
-      format.html { render action: "new" }  
+      if params[:current_tab] and params[:current_tab] =~ /tab(\d+)/
+        @tab = '#tab' + ($1.to_i + 1).to_s
+      end
+      render action: "new"
     end 
   end
 
