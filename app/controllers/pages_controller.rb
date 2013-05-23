8aff19f744e03a510dd04eed298bc69d6523132c
Ted Palmisciano <tpalmisciano@gmail.com>
2013-05-23 17:49:56 -0400

    put root redirect back /users/signup

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,7 +8,7 @@ class PagesController < ApplicationController
     if user_signed_in?
       redirect_to '/businesses'
     else
-      redirect_to '/users/sign_up?package_id=1'
+      redirect_to '/users/sign_up'
     end
   end
 
