33a54b28b4129d80b0068b20f473cdc986ff8864
Kazyyk <ubuntu@ip-10-245-115-145.ec2.internal>
2013-06-03 00:27:35 +0000

    check_email2

--- app/models/expressbusinessdirectory.rb
+++ app/models/expressbusinessdirectory.rb
@@ -14,5 +14,16 @@ class Expressbusinessdirectory < ClientData
     @link
  end  
 
+def self.check_email2(business)
+    @link = nil
+    CheckMail.get_link(business) do |mail|
+  	if mail.subject =~ /Verify Email for #{business.business_name} on ExpressBusinessDirectory/i
+		if mail.body.decoded =~ /(http:\/\/www.expressbusinessdirectory.com\/claimbusiness\/verifyBusinessEmail.aspx\S+)/i
+	          @link = $1
+	        end
+  	end
+    end
+    @link
+ end  
 	
 end
