faaf3093d761a557c17e7ed48f952beec0e6ad02
Ted Palmisciano <tpalmisciano@gmail.com>
2013-06-10 19:23:48 -0400

    submitting more changes for teh credit card processor

--- app/models/business_processor.rb
+++ app/models/business_processor.rb
@@ -1,21 +1,27 @@
 class BusinessProcessor
 
-  def create_a_business( user, package, coupon, credit_card )
+  def initialize( credit_card_processor ) 
+    @credit_card_processor = credit_card_processor 
+  end 
+
+
+  def create_a_business( user, package, coupon_code )
+
+    coupon = Coupon.where(:label_id => user.label.id, :code => coupon_code )
+    package.apply_coupon( coupon ) 
+
     transaction_event = TransactionEvent.new 
     transaction_event.package = package 
+    transaction_event.coupon = coupon 
     transaction_event.label = user.label 
     transaction_event.price =  package.price 
     transaction_event.original_price = package.original_price 
     transaction_event.monthly_fee = package.monthly_fee 
-    transaction_event.saved = package.saved 
-
-
-    cc = CreditCardProcessor.new( user.label, credit_card )
+    transaction_event.saved = package.saved
 
     # get business payment 
     # TODO transaction event     
-    payment = cc.charge( package.price * 100 ) 
-    payment.label = user.label 
+    payment = @credit_card_processor.charge( package.price * 100 ) 
 
     if payment.status == :success 
 
@@ -23,18 +29,42 @@ class BusinessProcessor
 
     end 
 
-    
     # set up a subscription
-    subscription  = cc.monthly_recurring_charge(  )     
+    subscription  = @credit_card_processor.monthly_recurring_charge(  ) 
 
-    # if subscription failed 
+    transacton_event.subscription = subscription  
 
+    if subscription.status == :success 
+      transaction_event.message = "Purchase complete" 
+      transaction_event.status = :success 
+    else 
+      transaction_event.message = subscription.message 
+      transaction_event.status  = subscription.status 
 
-    
-    # create the business 
+      @credit_card_processor.refund( payment )
+    end 
 
-    # return business  
+    # create the business
+    business = Business.new 
+    business.subscription = subscription 
+    business.user = user 
+    business.save :validate => false 
 
+    transaction_event.business = business 
+    transaction_event.save 
+
+    payment.business = business 
+    payment.transaction_event = transaction_event 
+    payment.label = user.label 
+    payment.save 
+
+    subscription.business = business 
+    subscription.package = package 
+    subscription.label = user.label 
+    subscription.transaction_event = transaction_event 
+    subscription.save
+
+    business 
   end 
 
 end 
