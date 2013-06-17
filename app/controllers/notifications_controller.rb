e36c949767fa076dbb4d1dce50feebf84f86f40e
Ted Palmisciano <tpalmisciano@gmail.com>
2013-06-17 17:02:17 -0400

    finished adding notifications panel

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -2,9 +2,8 @@ class NotificationsController < ApplicationController
   before_filter :authenticate_user!
 
   def index 
-    @notifications = Notification.where(:business_id => params[:busines_id] )
-    render 'index', layout: false 
+    @notifications = Notification.where(:business_id => params[:business_id])
+    render 'index', layout: false
   end 
 
-
-end 
+end
