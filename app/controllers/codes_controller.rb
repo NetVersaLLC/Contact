5fe39b649b1decb8d3cac11c324f2f75d9916da5
Ted Palmisciano <tpalmisciano@gmail.com>
2013-06-20 12:46:29 -0400

    added site_code to codes contorller for desktop to poll

--- app/controllers/codes_controller.rb
+++ app/controllers/codes_controller.rb
@@ -1,6 +1,8 @@
 class CodesController < ApplicationController 
 
   def new 
+    Code.delete_all(:business_id => params[:business_id], :site_name => params[:site_name]  ) 
+
     @code = Code.new 
     @code.business = Business.find(params[:business_id]) 
     @code.site_name = params[:site_name]
@@ -12,6 +14,15 @@ class CodesController < ApplicationController
     @code.save 
     flash[:notice] = 'Code saved!' 
     redirect_to business_path(@code.business)
+  end 
+
+  def site_code
+    @code = Code.where( :business_id => params[:business_id], :site_name => params[:site_name] ).first
+    if @code.nil? 
+      render :nothing => true, :status => :not_found
+    else 
+      render :json => @code
+    end 
   end 
 
 end 
