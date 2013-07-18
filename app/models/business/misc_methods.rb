131e963ba6bec713770ac368835e1e345e76dfa5
JoshWillis <blue74dot@gmail.com>
2013-07-18 23:54:08 +0000

    Removing utils from report

--- app/models/business/misc_methods.rb
+++ app/models/business/misc_methods.rb
@@ -84,6 +84,7 @@ module Business::MiscMethods
         site = citation_site_hash[p.site]
 
         if site.nil?
+          next if p.site == "Utils"
           non_account_data.push [p.site, 'Name not on citation list']
           next
         end
