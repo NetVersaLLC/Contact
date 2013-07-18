75bd6765a59427e9061d343617833e7556474440
JoshWillis <blue74dot@gmail.com>
2013-07-18 19:28:15 +0000

    Fixing Insiderpage[s] scan error

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,7 +10,7 @@ class Report < ActiveRecord::Base
     uri = URI('http://reports.savilo.com/TownCenter/leads.php')
     res = Net::HTTP.post_form(uri, {:phone => self.phone, :zip => self.zip, :business_name => self.business})
 
-    site_list     = %w/Google Yahoo Yelp Cornerstonesworld Discoverourtown Expressbusinessdirectory Ibegin Localizedbiz Yellowee Tupalo Localpages Getfave Uscity Insiderpages Zippro Gomylocal Uscity Kudzu Snoopitnow Justclicklocal/
+    site_list     = %w/Google Yahoo Yelp Cornerstonesworld Discoverourtown Expressbusinessdirectory Ibegin Localizedbiz Yellowee Tupalo Localpages Getfave Uscity Insiderpage Zippro Gomylocal Uscity Kudzu Snoopitnow Justclicklocal/
     site_list.each do |site|
       self.site = site
       scan        = Scanner.new(self)
