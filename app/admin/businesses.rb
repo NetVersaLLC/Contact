4642a77297f95d28192d4def63aff4b531d099ff
Ali Hassan <ali.hassan@virtual-force.com>
2013-07-19 15:08:28 +0500

    Removed new business button from business.

--- app/admin/businesses.rb
+++ app/admin/businesses.rb
@@ -63,16 +63,16 @@ ActiveAdmin.register Business do
       end
     end
 
-    def new
-      @business = Business.new(params[:business])
-      @business.user = current_user
-      if @business.save
-        flash[:notice] = "Business created"
-      else
-        flash[:alert] = "The system failed to create your business."
-      end
-      redirect_to admin_businesses_path
-    end
+    #def new
+    #  @business = Business.new(params[:business])
+    #  @business.user = current_user
+    #  if @business.save
+    #    flash[:notice] = "Business created"
+    #  else
+    #    flash[:alert] = "The system failed to create your business."
+    #  end
+    #  redirect_to admin_businesses_path
+    #end
 
     def show
       @business = Business.find(params[:id])
