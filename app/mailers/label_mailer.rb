e052a03d03efae6a89fecbc427138dee8e9ebeac
Ted Palmisciano <ted@tpalmconsulting.com>
2013-07-19 17:08:13 -0400

    new labelmailer

--- app/mailers/label_mailer.rb
+++ app/mailers/label_mailer.rb
@@ -3,6 +3,6 @@ class LabelMailer < ActionMailer::Base
 
   def sync_disabled_email(label)
     @label = label
-    mail(:to => label.mail_from, :subject => "Sync disabled due to insufficient funds."
+    mail(:to => label.mail_from, :subject => "Sync disabled due to insufficient funds.")
   end
 end
