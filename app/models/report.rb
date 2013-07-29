991ac001300874355bbaf81848bd527c72b17fb8
JoshWillis <blue74dot@gmail.com>
2013-07-29 19:21:20 +0000

    Updating Scan server site list

--- app/models/report.rb
+++ app/models/report.rb
@@ -10,7 +10,7 @@ class Report < ActiveRecord::Base
     uri = URI('http://reports.savilo.com/TownCenter/leads.php')
     res = Net::HTTP.post_form(uri, {:phone => self.phone, :zip => self.zip, :business_name => self.business})
 
-    site_list     = %w/Google Yahoo Yelp Cornerstonesworld Discoverourtown Expressbusinessdirectory Ibegin Localizedbiz Yellowee Tupalo Localpages Getfave Insiderpage Zippro Gomylocal Uscity Kudzu Snoopitnow Justclicklocal Businessdb Ebusinesspage/
+    site_list     = %w/Google Yahoo Yelp Bing Facebook Foursquare Citisquare Ebusinesspages Expressbusinessdirectory Getfave Hotfrog Ibegin Insiderpages Localizedbiz Showmelocal Uscity Yellowassistance Ziplocal Zippro/
     site_list.each do |site|
       self.site = site
       scan        = Scanner.new(self)
