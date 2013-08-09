ca9acf354576c8af8335f80305e24374077a9e66
Ted Palmisciano <ted@tpalmconsulting.com>
2013-08-09 16:41:46 -0400

    Set attach scan and reports to the current label on request

--- app/models/report.rb
+++ app/models/report.rb
@@ -2,7 +2,7 @@ class Report < ActiveRecord::Base
   include Backburner::Performable
   queue "reports"
   queue_priority 1000
-  attr_accessible :business, :business_id, :completed_at, :name, :phone, :site, :started_at, :zip
+  attr_accessible :business, :business_id, :completed_at, :name, :phone, :site, :started_at, :zip,  :label_id
   has_many :scans
 
   def perform
@@ -11,7 +11,8 @@ class Report < ActiveRecord::Base
     res = Net::HTTP.post_form(uri, {:phone => self.phone, :zip => self.zip, :business_name => self.business})
 
     threads       = []
-    site_list     = %w/Google Yahoo Yelp Cornerstonesworld Discoverourtown Expressbusinessdirectory Ibegin Localizedbiz Yellowee Tupalo Localpages Getfave Insiderpage Zippro Gomylocal Uscity Kudzu Snoopitnow Justclicklocal Businessdb Ebusinesspage/
+    #site_list = %w/Google Yahoo Yelp Cornerstonesworld Discoverourtown Expressbusinessdirectory Ibegin Localizedbiz Yellowee Tupalo Localpages Getfave Insiderpage Zippro Gomylocal Uscity Kudzu Snoopitnow Justclicklocal Businessdb Ebusinesspage/
+    site_list = %w/Google Yahoo Yelp Bing Facebook Foursquare Cornerstonesworld Citisquare Ebusinesspages Expressbusinessdirectory Getfave Hotfrog Ibegin Insiderpages Localizedbiz Showmelocal Uscity Yellowassistance Zippro/
     site_list.each do |site|
       threads << Thread.new do
         scan        = Scanner.new(self, site)
@@ -25,13 +26,14 @@ class Report < ActiveRecord::Base
     return self
   end
 
-  def self.generate(business, zip, phone, package_id, ident)
+  def self.generate(business, zip, phone, package_id, ident, label) 
     report = Report.create do |r|
       r.business   = business
       r.zip        = zip
       r.phone      = phone
       r.package_id = package_id
       r.ident      = ident
+      r.label      = label
     end
     report.async.perform
     # Backburner.enqueue Report, report.id
