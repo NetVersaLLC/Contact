32c005a5e6e7aa36ece0dd6925156b22b5d34832
Jonathan Jeffus <jjeffus@gmail.com>
2013-08-13 15:58:21 -0700

    updating category listings

--- config/deploy/worker.rb
+++ config/deploy/worker.rb
@@ -1,3 +1,3 @@
 set  :rails_env ,'production'
 set  :branch    ,'production'
-server 'ec2-54-226-115-55.compute-1.amazonaws.com', :app, :web, :primary => true
+server 'ec2-54-226-115-55.compute-1.amazonaws.com', :app, :web, :db, :primary => true
