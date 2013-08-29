b42405be214c8ed76b4c4bbc2c1b27376ff53d17
Coin Age <coinage7@gmail.com>
2013-08-29 12:08:35 -0700

    update for verify

--- app/models/bizzspot.rb
+++ app/models/bizzspot.rb
@@ -1,4 +1,24 @@
 class Bizzspot < ClientData
   attr_accessible :email, :username
   virtual_attr_accessor :password  
+
+  def self.check_email(business)
+    @link = nil
+    CheckMail.get_link(business) do |mail|
+      if mail.subject =~ /BizzSpot Account Activation/i
+        mail.parts.map do |p|
+          if p.content_type =~ /text\/html/
+            nok = Nokogiri::HTML(p.decoded)
+            nok.xpath("//a").each do |link|
+              if link.attr('href') =~ /http:\/\/www.bizzspot.com\/*/
+                @link = link.attr('href')
+              end
+            end
+          end
+        end
+      end
+    end
+    STDERR.puts "Bizzspot link: #{@link}"
+    @link
+end
 end
