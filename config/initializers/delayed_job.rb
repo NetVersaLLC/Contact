4e0d9d78e7c791323c535adf95c7eb52db89b821
nixoid <andrey.zamovskiy@skykillers.com>
2013-10-03 14:40:06 -0700

    optimized delayed job settings

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,8 +1,8 @@
 Delayed::Worker.logger = Logger.new(File.join(Rails.root, 'log', 'dj.log'))
 Delayed::Worker.sleep_delay = 2
-Delayed::Worker.destroy_failed_jobs = false
-Delayed::Worker.max_run_time = 10.seconds
-Delayed::Worker.max_attempts = 3
+Delayed::Worker.destroy_failed_jobs = true
+Delayed::Worker.max_run_time = 5.seconds
+Delayed::Worker.max_attempts = 1
 Delayed::Worker.default_priority = 5
 Delayed::Worker.read_ahead = 5
 Delayed::Worker.default_queue_name = 'scans'
