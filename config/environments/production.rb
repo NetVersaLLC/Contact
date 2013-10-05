cad0bbbb5bc8eee9eecf3ec5cb7839f708d21b3b
nixoid <andrey.zamovskiy@skykillers.com>
2013-10-04 23:56:46 -0400

    fixed server api token for production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -36,7 +36,7 @@ Contact::Application.configure do
   # used to authenticate scan server callbacks
   config.scan_server_uri = 'http://ec2-50-17-135-19.compute-1.amazonaws.com/'
   config.scan_server_api_username = 'api'
-  config.scan_server_api_token = '892457gh9q87fah98ef7hq987harhq9w87eh8'
+  config.scan_server_api_token = '13fc9e78f643ab9a2e11a4521479fdfe'
   # period to wait before resending task that is waiting for result to scanserver again
   config.scan_task_resend_interval = 10.seconds
   # how much time to wait before considering scan as failed
