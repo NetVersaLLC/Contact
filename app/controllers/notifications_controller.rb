a3e0872e1ab4b8caea2d5c3581a492eea5a01ca5
Ted Palmisciano <ted@tpalmconsulting.com>
2013-10-09 14:32:20 -0400

    added Action Items (notifications) to active admin

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,53 +1,57 @@
-class NotificationsController < ApplicationController
-  before_filter :authenticate_user!
+class NotificationsController < InheritedResources::Base
+  #before_filter :authenticate_user!
+  load_and_authorize_resource 
 
-  def index
-    @notifications = Notification.where(:business_id => params[:business_id])
-    respond_to do |format|
-      format.html { render 'index', layout: false }
-      format.json do
-        html = render_to_string 'list', layout: false, formats: [:html]
-        data = {
-          :html => html,
-          :notifications => @notifications
-        }
-        render json: data
-      end
-    end
-  end
+  respond_to :html, :json
+  actions :all
 
-  def edit
-    @notification = Notification.find(params[:id])
-    respond_to do |format|
-      format.html { render 'edit', layout: false }
-    end
-  end
+#  def index
+#    @notifications = Notification.where(:business_id => params[:business_id])
+#    respond_to do |format|
+#      format.html { render 'index', layout: false }
+#      format.json do
+#        html = render_to_string 'list', layout: false, formats: [:html]
+#        data = {
+#          :html => html,
+#          :notifications => @notifications
+#        }
+#        render json: data
+#      end
+#    end
+#  end
 
-  def update
-    @notification = Notification.find(params[:id])
-    @notification.update_attributes(params[:notification])
-    render :json => @notification
-  end
-
-  def new
-    @notification = Notification.new
-    respond_to do |format|
-      format.html { render 'edit', layout: false }
-    end
-  end
-
-  def show
-    @notification = Notification.find(params[:id])
-  end
-
-  def create
-    @notification = Notification.create(params[:notification])
-    render json: @notification
-  end
-
-  def destroy
-    Notification.find(params[:id]).delete
-    render json: true
-  end
+#  def edit
+#    @notification = Notification.find(params[:id])
+#    respond_to do |format|
+#      format.html { render 'edit', layout: false }
+#    end
+#  end
+#
+#  def update
+#    @notification = Notification.find(params[:id])
+#    @notification.update_attributes(params[:notification])
+#    render :json => @notification
+#  end
+#
+#  def new
+#    @notification = Notification.new
+#    respond_to do |format|
+#      format.html { render 'edit', layout: false }
+#    end
+#  end
+#
+#  def show
+#    @notification = Notification.find(params[:id])
+#  end
+#
+#  def create
+#    @notification = Notification.create(params[:notification])
+#    render json: @notification
+#  end
+#
+#  def destroy
+#    Notification.find(params[:id]).delete
+#    render json: true
+#  end
 
 end
--- app/controllers/site_profiles_controller.rb
+++ app/controllers/site_profiles_controller.rb
@@ -2,6 +2,7 @@ class SiteProfilesController < InheritedResources::Base
   load_and_authorize_resource 
   respond_to :html #,:xml, :json
   actions :all
+  add_breadcrumb 'Site Profiles', :site_profiles_url
 
 
 end 
