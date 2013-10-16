466f793d9681392a2174324699fd36601e6c50bd
Ted Palmisciano <ted@tpalmconsulting.com>
2013-10-16 17:52:52 -0400

    fix for activeadmin pagination

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -1,3 +1,16 @@
+if defined?(WillPaginate)
+  module WillPaginate
+    module ActiveRecord
+      module RelationMethods
+        def per(value = nil) per_page(value) end
+        def total_count() count end
+      end
+    end
+    module CollectionMethods
+      alias_method :num_pages, :total_pages
+    end
+  end
+end
 ActiveAdmin.setup do |config|
   def authenticate_admin!
     redirect_to destroy_user_session_path unless current_user.reseller?
