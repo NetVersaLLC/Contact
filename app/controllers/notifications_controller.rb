866565e07e637ad17abb9d6a249c5f0cb864659f
Ted Palmisciano <ted@tpalmconsulting.com>
2013-10-18 10:18:08 -0400

    add pagination to notfications page

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,50 +5,11 @@ class NotificationsController < InheritedResources::Base
   respond_to :html, :json
   actions :all
 
-#  def index
-#    @notifications = Notification.where(:business_id => params[:business_id])
-#    respond_to do |format|
-#      format.html { render 'index', layout: false }
-#      format.json do
-#        html = render_to_string 'list', layout: false, formats: [:html]
-#        data = {
-#          :html => html,
-#          :notifications => @notifications
-#        }
-#        render json: data
-#      end
-#    end
-#  end
+  protected
+    def collection
+      @notifications = end_of_association_chain.includes(:business).paginate(page: params[:page])
+    end 
 
-#  def edit
-#    @notification = Notification.find(params[:id])
-#    respond_to do |format|
-#      format.html { render 'edit', layout: false }
-#    end
-#  end
-#
-#  def update
-#    @notification = Notification.find(params[:id])
-#    @notification.update_attributes(params[:notification])
-#    render :json => @notification
-#  end
-#
-#  def new
-#    @notification = Notification.new
-#    respond_to do |format|
-#      format.html { render 'edit', layout: false }
-#    end
-#  end
-#
-#  def show
-#    @notification = Notification.find(params[:id])
-#  end
-#
-#  def create
-#    @notification = Notification.create(params[:notification])
-#    render json: @notification
-#  end
-#
 #  def destroy
 #    Notification.find(params[:id]).delete
 #    render json: true
