0837573b85f2f3e9cd9233341619bb9b87e3be9d
Ted Palmisciano <ted@tpalmconsulting.com>
2013-10-24 10:07:03 -0400

    cleaning up show pages and adding a bit of styling

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,7 @@ class Site < ActiveRecord::Base
   attr_accessible :logo
   has_attached_file :logo, styles: { :thumb => "32x32>" }, default_url: "/assets/missing_logo.jpg"
 
-  attr_accessible :alexa_us_traffic_rank, :founded, :notes, :owner, :page_rank, :site, :traffic_stats, :url, :enabled_for_scan, :enabled, :technical_notes
+  attr_accessible :alexa_us_traffic_rank, :founded, :notes, :owner, :page_rank, :name, :traffic_stats, :domain, :enabled_for_scan, :enabled, :technical_notes
   has_many :payloads
 
   def self.by_name(name)
