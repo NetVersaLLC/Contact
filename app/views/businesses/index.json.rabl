95a68224f2d1bcfd552d973a34899125b638878c
Jonathan Jeffus <jjeffus@gmail.com>
2013-10-24 10:40:24 -0700

    removing rabl

--- app/views/businesses/index.json.rabl
+++
@@ -1,6 +0,0 @@
-object @business
-attributes :id, :name, :contact, :phone,
-    :alternate_phone, :fax, :address, :address2,
-    :city, :state, :zip, :website, :email, :approved,
-    :first_name, :middle_initial, :last_name
-node(:yelp_category) { |business| YelpCategory.find(business.yelp_category_id).to_list }
--- db/schema.rb
+++ db/schema.rb
@@ -158,6 +158,17 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
 
   add_index "bings", ["business_id"], :name => "index_bings_on_business_id"
 
+  create_table "bizzspots", :force => true do |t|
+    t.integer  "business_id"
+    t.string   "email"
+    t.string   "username"
+    t.text     "secrets"
+    t.datetime "force_update"
+    t.boolean  "do_not_sync",  :default => false
+    t.datetime "created_at",                      :null => false
+    t.datetime "updated_at",                      :null => false
+  end
+
   create_table "booboos", :force => true do |t|
     t.integer  "user_id"
     t.integer  "business_id"
@@ -887,6 +898,7 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
     t.integer  "yellowtalk_category_id"
     t.integer  "yellowwiz_category_id"
     t.integer  "citydata_category_id"
+    t.integer  "meetlocalbiz_category_id"
   end
 
   add_index "google_categories", ["name"], :name => "index_google_categories_on_name"
@@ -1393,6 +1405,28 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
 
   add_index "matchpoints", ["business_id"], :name => "index_matchpoints_on_business_id"
 
+  create_table "meetlocalbiz_categories", :force => true do |t|
+    t.integer  "parent_id"
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "meetlocalbiz_categories", ["name"], :name => "index_meetlocalbiz_categories_on_name"
+  add_index "meetlocalbiz_categories", ["parent_id"], :name => "index_meetlocalbiz_categories_on_parent_id"
+
+  create_table "meetlocalbizs", :force => true do |t|
+    t.integer  "business_id"
+    t.text     "username"
+    t.text     "email"
+    t.text     "secrets"
+    t.datetime "force_update"
+    t.datetime "created_at",               :null => false
+    t.datetime "updated_at",               :null => false
+    t.integer  "meetlocalbiz_category_id"
+    t.boolean  "do_not_sync"
+  end
+
   create_table "merchantcircle_categories", :force => true do |t|
     t.integer  "parent_id"
     t.string   "name"
@@ -1474,8 +1508,8 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
     t.datetime "force_update"
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
-    t.string   "username"
     t.boolean  "do_not_sync",  :default => false
+    t.string   "username"
   end
 
   create_table "notifications", :force => true do |t|
@@ -1763,11 +1797,11 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
     t.boolean  "enabled_for_scan",      :default => false
     t.boolean  "enabled",               :default => true
     t.text     "technical_notes"
-    t.string   "login_url"
     t.string   "logo_file_name"
     t.string   "logo_content_type"
     t.integer  "logo_file_size"
     t.datetime "logo_updated_at"
+    t.string   "login_url"
   end
 
   add_index "sites", ["name"], :name => "index_sites_on_name"
@@ -1827,7 +1861,7 @@ ActiveRecord::Schema.define(:version => 20131022215151) do
     t.integer  "monthly_fee"
     t.string   "status"
     t.integer  "transaction_event_id"
-    t.datetime "label_last_billed_at", :default => '2013-08-16 20:11:27'
+    t.datetime "label_last_billed_at", :default => '2013-08-24 16:11:57'
   end
 
   add_index "subscriptions", ["package_id"], :name => "index_subscriptions_on_package_id"
