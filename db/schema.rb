96290bc010925c736ff163b448072c65bb459bb6
Jonathan Jeffus <jjeffus@gmail.com>
2013-11-03 07:57:50 -0800

    fixing payloads import

--- db/schema.rb
+++ db/schema.rb
@@ -158,17 +158,6 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
 
   add_index "bings", ["business_id"], :name => "index_bings_on_business_id"
 
-  create_table "bizzspots", :force => true do |t|
-    t.integer  "business_id"
-    t.string   "email"
-    t.string   "username"
-    t.text     "secrets"
-    t.datetime "force_update"
-    t.boolean  "do_not_sync",  :default => false
-    t.datetime "created_at",                      :null => false
-    t.datetime "updated_at",                      :null => false
-  end
-
   create_table "booboos", :force => true do |t|
     t.integer  "user_id"
     t.integer  "business_id"
@@ -901,7 +890,6 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
     t.integer  "yellowtalk_category_id"
     t.integer  "yellowwiz_category_id"
     t.integer  "citydata_category_id"
-    t.integer  "meetlocalbiz_category_id"
   end
 
   add_index "google_categories", ["name"], :name => "index_google_categories_on_name"
@@ -1409,28 +1397,6 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
 
   add_index "matchpoints", ["business_id"], :name => "index_matchpoints_on_business_id"
 
-  create_table "meetlocalbiz_categories", :force => true do |t|
-    t.integer  "parent_id"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
-  add_index "meetlocalbiz_categories", ["name"], :name => "index_meetlocalbiz_categories_on_name"
-  add_index "meetlocalbiz_categories", ["parent_id"], :name => "index_meetlocalbiz_categories_on_parent_id"
-
-  create_table "meetlocalbizs", :force => true do |t|
-    t.integer  "business_id"
-    t.text     "username"
-    t.text     "email"
-    t.text     "secrets"
-    t.datetime "force_update"
-    t.datetime "created_at",               :null => false
-    t.datetime "updated_at",               :null => false
-    t.integer  "meetlocalbiz_category_id"
-    t.boolean  "do_not_sync"
-  end
-
   create_table "merchantcircle_categories", :force => true do |t|
     t.integer  "parent_id"
     t.string   "name"
@@ -1512,8 +1478,8 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
     t.datetime "force_update"
     t.datetime "created_at",                      :null => false
     t.datetime "updated_at",                      :null => false
-    t.boolean  "do_not_sync",  :default => false
     t.string   "username"
+    t.boolean  "do_not_sync",  :default => false
   end
 
   create_table "notifications", :force => true do |t|
@@ -1577,13 +1543,6 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
     t.boolean  "do_not_sync",       :default => false
   end
 
-  create_table "payload_categories", :force => true do |t|
-    t.string   "name"
-    t.integer  "position"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "payloads", :force => true do |t|
     t.string   "name"
     t.boolean  "active",         :default => false
@@ -1626,17 +1585,6 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
   add_index "payments", ["business_id"], :name => "index_payments_on_business_id"
   add_index "payments", ["label_id"], :name => "index_payments_on_label_id"
   add_index "payments", ["transaction_id"], :name => "index_payments_on_transaction_id"
-
-  create_table "pings", :force => true do |t|
-    t.integer  "user_id"
-    t.integer  "business_id"
-    t.string   "message"
-    t.datetime "created_at",  :null => false
-    t.datetime "updated_at",  :null => false
-  end
-
-  add_index "pings", ["business_id"], :name => "index_pings_on_business_id"
-  add_index "pings", ["user_id"], :name => "index_pings_on_user_id"
 
   create_table "primeplace_categories", :force => true do |t|
     t.integer  "parent_id"
@@ -1829,6 +1777,10 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
     t.boolean  "enabled",               :default => true
     t.text     "technical_notes"
     t.string   "login_url"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
   end
 
   add_index "sites", ["name"], :name => "index_sites_on_name"
@@ -1888,7 +1840,7 @@ ActiveRecord::Schema.define(:version => 20131102224708) do
     t.integer  "monthly_fee"
     t.string   "status"
     t.integer  "transaction_event_id"
-    t.datetime "label_last_billed_at", :default => '2013-07-15 20:43:13'
+    t.datetime "label_last_billed_at", :default => '2013-08-16 20:11:27'
   end
 
   add_index "subscriptions", ["package_id"], :name => "index_subscriptions_on_package_id"
--- lib/tasks/payloads/import.rake
+++ lib/tasks/payloads/import.rake
@@ -1,4 +1,45 @@
 namespace :payloads do
   task :import => :environment do
+    Payload.delete_all
+    db = Rails.configuration.database_configuration
+    signup = Mode.find_by_name("SignUp")
+    get_children = lambda do |code|
+      if match = code.match(/self\.start\(?\s*["']([^"'+]+)["']\s*\)?/i)
+        return match.captures
+      else
+        return []
+      end
+    end
+    parse_filename = lambda do |file|
+      file.match(/sites\/([^\/]+)\/([^\/]+)/).captures
+    end
+    set_sitename = lambda do |site_name|
+      site = Site.find_by_name(site_name)
+      if site.nil?
+        print "What is the right site name for: #{site_name}: "
+        site_name = STDIN.gets.strip
+        site = Site.find_by_name(site_name)
+      end
+      return site
+    end
+    ActiveRecord::Base.connection.execute("ALTER TABLE #{db[Rails.env]['database']}.payloads SET auto_increment=1");
+    Dir.glob("sites/*/SignUp/client_script.rb").each do |script|
+      data_generator_file = script.split("/")[0 .. -2].join("/") + "/data_generator.rb"
+      data_generator = nil
+      if File.exists? data_generator_file
+        data_generator = File.read data_generator_file
+      end
+      client_script = File.read script
+      site_name, payload_name = *parse_filename(script)
+      site = nil
+      while site == nil
+        site = set_sitename(site_name)
+      end
+      Payload.create do |p|
+        p.site_id = site.id
+        p.client_script = script
+        p.name = name
+      end
+    end
   end
 end
