09145a35e0f2a5262e0f03d1c95058d9d597e8ab
nixoid <andrey.zamovskiy@skykillers.com>
2013-11-02 22:47:24 -0700

    deployment script should now restart daemons properly

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,3 +1,6 @@
 set  :rails_env ,'production'
 set  :branch    ,'staging'
 server 'staging.netversa.com', :app, :web, :db, :primary => true
+
+after 'deploy:migrations',  'daemons:stop'
+after 'daemons:stop',  'daemons:start'
