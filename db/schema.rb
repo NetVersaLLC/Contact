2cbf870cf7a7b371edfefb75a48bfbd9d8dbe79b
Ted Palmisciano <ted@tpalmconsulting.com>
2013-11-05 23:12:37 -0500

    lazy integration of sugarcrm

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20131106002618) do
+ActiveRecord::Schema.define(:version => 20131106035018) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
@@ -1032,6 +1032,15 @@ ActiveRecord::Schema.define(:version => 20131106002618) do
 
   add_index "insider_pages", ["business_id"], :name => "index_insider_pages_on_business_id"
 
+  create_table "insiderpages", :force => true do |t|
+    t.integer  "business_id"
+    t.string   "email"
+    t.text     "secrets"
+    t.datetime "force_update"
+    t.datetime "created_at",   :null => false
+    t.datetime "updated_at",   :null => false
+  end
+
   create_table "jaydes", :force => true do |t|
     t.integer  "business_id"
     t.text     "secrets"
@@ -1149,6 +1158,9 @@ ActiveRecord::Schema.define(:version => 20131106002618) do
     t.string   "support_email"
     t.string   "support_phone"
     t.text     "report_email_body"
+    t.string   "crm_url"
+    t.string   "crm_username"
+    t.string   "crm_password"
   end
 
   add_index "labels", ["domain"], :name => "index_labels_on_domain"
@@ -1815,6 +1827,10 @@ ActiveRecord::Schema.define(:version => 20131106002618) do
     t.boolean  "enabled_for_scan",      :default => false
     t.boolean  "enabled",               :default => true
     t.text     "technical_notes"
+    t.string   "logo_file_name"
+    t.string   "logo_content_type"
+    t.integer  "logo_file_size"
+    t.datetime "logo_updated_at"
     t.string   "login_url"
     t.string   "model"
   end
@@ -1877,7 +1893,7 @@ ActiveRecord::Schema.define(:version => 20131106002618) do
     t.integer  "monthly_fee"
     t.string   "status"
     t.integer  "transaction_event_id"
-    t.datetime "label_last_billed_at", :default => '2013-07-13 21:46:39'
+    t.datetime "label_last_billed_at", :default => '2013-07-09 21:48:55'
   end
 
   add_index "subscriptions", ["package_id"], :name => "index_subscriptions_on_package_id"
