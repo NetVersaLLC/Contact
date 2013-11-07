441707bd03a497c6e9178190edc77b6172fb1e0f
Ted Palmisciano <ted@tpalmconsulting.com>
2013-11-07 15:01:25 -0500

    Assign the Lead to a Sales Person, if found, when posting to SugarCRM. Added a reset button to the scan form.

--- app/models/sugar.rb
+++ app/models/sugar.rb
@@ -7,7 +7,10 @@ class Sugar
   end
 
   def generate_lead( business_name, first_name, last_name, phone, email, zip, referral, package, ident, label)
+
+      sales_person = connection::User.all( {conditions: { user_name: "LIKE '%#{referral}'"}} ).first
       lead = connection::Lead.new
+
       lead.account_name = business_name
       lead.first_name = first_name
       lead.last_name = last_name
@@ -17,6 +20,9 @@ class Sugar
       lead.opportunity_amount = "#{package.price} - #{package.monthly_fee}/month"
       lead.refered_by = "#{label.name} - #{referral}"
       lead.lead_source_description = "report id=#{ident} - package id=#{package.id}"
+
+      lead.assigned_user_id = sales_person.id if sales_person
+
       lead.save
   end
 
