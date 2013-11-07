35873b7f3a221e2e81ffdd48c033ba85769631b0
Ted Palmisciano <ted@tpalmconsulting.com>
2013-11-07 15:22:37 -0500

    display subscription rates in dollars and cents instead of cents

--- app/models/label_processor.rb
+++ app/models/label_processor.rb
@@ -141,6 +141,7 @@ class LabelProcessor
     subscription.business = business 
     subscription.package = package 
     subscription.label = @label
+    subscription.initial_fee = payment.amount
 
     transaction_event.subscription = subscription  
 
