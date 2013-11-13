07a0e94dbc9573baf33b906b76e691090cb6e830
Andrey Zamovskiy <nihuhoid@gmail.com>
2013-11-13 07:24:02 -0800

    testing

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,7 +3,7 @@ set  :branch    ,'staging'
 server 'staging.netversa.com', :app, :web, :db, :primary => true
 
 after 'deploy:migrations' do
-  print 'hello world'
+  print 'hello world2'
 end
 #after 'deploy:migrations',  'daemons:stop'
 #after 'daemons:stop',  'daemons:start'
