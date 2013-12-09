9f523b373e1657fa3ccf4428bf5816170430f450
Ted Palmisciano <ted@tpalmconsulting.com>
2013-12-09 13:05:34 -0500

    Added fields to purchase email receipt.  Fixed credit card brand issue with mastercard instead of master

--- app/mailers/receipt_mailer.rb
+++ app/mailers/receipt_mailer.rb
@@ -1,9 +1,11 @@
 class ReceiptMailer < ActionMailer::Base 
-  def purchase_email( business ) 
+  def purchase_email( business, credit_card ) 
     @business = business
+    @credit_card = credit_card
+    @brand = { 'master' => 'MasterCard', 'visa' => 'Visa', 'american_express' => 'American Express', 'discover' => 'Discover'}[ credit_card[:brand] ]
 
     to = business.user.email
-    from = business.label.support_email
+    from = business.label.mail_from
     subject = business.label.name + " Purchase"
 
     mail( to: to, from: from, subject: subject) 
