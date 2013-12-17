9d85bd873951ad213f3796ce644aea2222197a47
Jahangir Zinedine <jzinedine@gmail.com>
2013-12-18 03:16:32 +0330

    Merge branch 'jani-jobs-refactored' of https://github.com/NetVersaLLC/Contact into jani-jobs-refactored
    Conflicts:
    	db/schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-
 ActiveRecord::Schema.define(:version => 20131127170201) do
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
@@ -1929,13 +1928,7 @@ ActiveRecord::Schema.define(:version => 20131127170201) do
     t.integer  "monthly_fee"
     t.string   "status"
     t.integer  "transaction_event_id"
-<<<<<<< HEAD
     t.datetime "label_last_billed_at", :default => '2013-07-13 21:46:39'
-||||||| merged common ancestors
-    t.datetime "label_last_billed_at", :default => '2013-10-05 18:43:01'
-=======
-    t.datetime "label_last_billed_at", :default => '2013-10-10 08:25:20'
->>>>>>> Correct the Job.success method to update the mode and write a spec for it
   end
 
   add_index "subscriptions", ["package_id"], :name => "index_subscriptions_on_package_id"
