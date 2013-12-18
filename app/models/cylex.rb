22c24b33627d3c79c86e60464a055ecea6619a77
Kazyyk <Asphexis@gmail.com>
2013-12-18 09:30:31 +0000

    Added support for :listing_url

--- app/models/cylex.rb
+++ app/models/cylex.rb
@@ -1,5 +1,5 @@
 class Cylex < ClientData  
-  attr_accessible :business_id, :created_at, :email, :force_update, :secretsi
+  attr_accessible :business_id, :created_at, :email, :listing_url, :force_update, :secretsi
   virtual_attr_accessor :password
 #  validates :password,
 #        :presence => true
