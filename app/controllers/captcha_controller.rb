3f2b6423e6d114cc7ca715d37106f6d507570894
Ted Palmisciano <ted@tpalmconsulting.com>
2013-12-26 16:01:56 -0500

    added verification notifications to the dashboard

--- app/controllers/captcha_controller.rb
+++ app/controllers/captcha_controller.rb
@@ -5,31 +5,6 @@ class CaptchaController < ApplicationController
   skip_before_filter :authenticate_user!
   skip_before_filter :verify_authenticity_token
 
-  def new 
-  end 
-
-  def decode 
-    business = Business.where(:id => params[:business_id]).first
-    if business.captcha_solves > 0 and business.user_id == current_user.id
-      file = Tempfile.new('temp')
-      file.binmode
-      file.write(params['image'].read())
-      file.close
-      business.captcha_solves -= 1
-      business.save :validate => false
-      client = DeathByCaptcha.http_client('netversa', 'GSbjMIe9Ja')
-      res = client.decode file.path
-      if res and res.length > 0 and res['text']
-        @captcha_text= res['text']
-      else
-        @captcha_text= 'error'
-      end
-    else
-        @captcha_text= 'not authorized'
-    end
-    render :show
-  end
-
   def recaptcha
     business = Business.where(:id => params[:business_id]).first
     if business.captcha_solves > 0 and business.user_id == current_user.id
