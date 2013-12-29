15d21f05f3a9495297568c5bf34b7b91840329d6
Kazyyk <Asphexis@gmail.com>
2013-12-29 12:36:24 -0500

    Fixed birthday method
    Fixed the birthday method hopefully for the last time

--- app/models/business/misc_methods.rb
+++ app/models/business/misc_methods.rb
@@ -169,7 +169,7 @@ module Business::MiscMethods
         self.contact_birthday = date
         self.save
       end
-      Date.strptime(self.contact_birthday, '%Y-%m-%d')
+      Date.strptime(self.contact_birthday, '%m/%d/%Y')
     end
 
     def report_xlsx
