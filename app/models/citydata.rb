32c6a36a2ac409e2460257ac08ea419fa8ea131f
Ted Palmisciano <ted@tpalmconsulting.com>
2014-01-03 14:42:47 -0500

    whitelist listing_urls on more accounts

--- app/models/citydata.rb
+++ app/models/citydata.rb
@@ -1,5 +1,5 @@
 class Citydata < ClientData
-  attr_accessible :email
+  attr_accessible :email, :listing_url
   virtual_attr_accessor :password
   belongs_to :citydata_category
   def has_categories? 
--- app/models/ziplocal.rb
+++ app/models/ziplocal.rb
@@ -1,5 +1,5 @@
 class Ziplocal  < ClientData 
-  attr_accessible :email
+  attr_accessible :email, :listing_url
   virtual_attr_accessor :password
   belongs_to            :ziplocal_category 
 end
