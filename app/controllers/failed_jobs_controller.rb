8ea4b8e4e02d4add029a1ef15c66950f108e9418
Ted Palmisciano <ted@tpalmconsulting.com>
2014-01-13 10:25:47 -0500

    second errors report page added

--- app/controllers/failed_jobs_controller.rb
+++ app/controllers/failed_jobs_controller.rb
@@ -2,6 +2,11 @@ class FailedJobsController < InheritedResources::Base
   before_filter :authenticate_reseller!
 
   def index 
-    @rows = FailedJob.errors_report
+    if params[:site] 
+      @rows = FailedJob.site_errors_report(params[:site])
+      render 'site_errors_report'
+    else 
+      @rows = FailedJob.errors_report
+    end 
   end 
 end 
--- app/models/failed_job.rb
+++ app/models/failed_job.rb
@@ -10,7 +10,7 @@ class FailedJob < JobBase
     FailedJob.connection.select_all("select sites.name, count(distinct failed_jobs.payload_id) as payloads_with_errors from failed_jobs inner join payloads on failed_jobs.payload_id = payloads.id inner join sites on sites.id = payloads.site_id group by sites.name order by sites.name")
   end
   def self.site_errors_report(site_name)
-    site = Site.where(name: site_name).first
-    rows = FailedJob.connection.select_all("select max(failed_jobs.id) as id, max(name) as name, count(failed_jobs.id) as count from failed_jobs where payload_id in (342,446,447,448,449,450,451,452,453) group by grouping_hash") 
+    payloads_search = Site.where(name: site_name).first.payloads.pluck(:id).join(',')
+    FailedJob.connection.select_all("select max(failed_jobs.id) as id, max(name) as name, max(status_message) as status_message, max(updated_at) as last_updated_at, count(distinct business_id) businesses_count from failed_jobs where payload_id in (#{payloads_search}) group by grouping_hash") 
   end 
 end
