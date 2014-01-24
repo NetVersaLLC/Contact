711433566d4c0c0b5aa739e7222104a6bf36f932
Ted Palmisciano <ted@tpalmconsulting.com>
2014-01-24 17:19:26 -0500

    added leaderboard

--- app/models/cost_center.rb
+++ app/models/cost_center.rb
@@ -7,4 +7,14 @@ class CostCenter < ActiveRecord::Base
   has_many :managers
   has_many :customer_service_agents
 
+  def leaderboard 
+    # TODO rewrite this!
+    sales_people_ids = managers.includes(:sales_people).map{|m| m.sales_people.pluck(:id)}.flatten.join(",")
+    ActiveRecord::Base.connection.select_all(
+      "select users.first_name, users.last_name, sum(businesses.id) as sold_count 
+      from businesses 
+      inner join users on users.id = businesses.sales_person_id
+      where businesses.created_at > '#{Time.now.beginning_of_week.to_date}' and sales_person_id in (#{sales_people_ids}) 
+      group by sales_person_id")
+  end 
 end 
