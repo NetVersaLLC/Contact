4f49f19b9e127a20fea33c1195d5228fc89339d5
Ted Palmisciano <ted@tpalmconsulting.com>
2014-01-31 17:00:58 -0500

    fixes for resolved errors leaderboard

--- app/controllers/failed_jobs_controller.rb
+++ app/controllers/failed_jobs_controller.rb
@@ -19,10 +19,10 @@ class FailedJobsController < ApplicationController
     reque = params[:inject].present? 
     number_of_errors_resolved = FailedJob.resolve_by_grouping_hash( params[:grouping_hash], reque)
 
-    current_user.rewards << Reward.new( points: number_of_errors * 100 ) if current_user.is_a? Administrator
+    current_user.rewards << Reward.new( points: number_of_errors_resolved * 100 ) if current_user.is_a? Administrator
 
     flash[:notice] = "#{number_of_errors_resolved} errors were updated."
-    redirect_to failed_jobs_path
+    redirect_to :back
   end 
   private 
     def authenticate_failed_jobs
