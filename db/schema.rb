6e2bd203bf4a7fd9a662601aaefe4a611d17f5f5
Ted Palmisciano <ted@tpalmconsulting.com>
2014-02-06 10:38:30 -0500

    merchantcircle category helper methods

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140131210655) do
+ActiveRecord::Schema.define(:version => 20140206152318) do
 
   create_table "accounts", :force => true do |t|
     t.integer  "business_id"
